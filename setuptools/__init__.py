61d2fd1d71e8940531c35327e6d91c41ddcd9dbd
PJ Eby <distutils-sig@python.org>
2005-07-07 02:10:14 +0000

    Bump release version
    --HG--
    branch : setuptools
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/sandbox/trunk/setuptools%4041089

--- setuptools/__init__.py
+++ setuptools/__init__.py
@@ -8,7 +8,7 @@ from distutils.core import Command as _Command
 from distutils.util import convert_path
 import os.path
 
-__version__ = '0.5a5'
+__version__ = '0.5a6'
 
 __all__ = [
     'setup', 'Distribution', 'Feature', 'Command', 'Extension', 'Require',
