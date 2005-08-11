b8878fdb9dc2bfe8733a852547f15ec167476752
PJ Eby <distutils-sig@python.org>
2005-08-11 14:58:54 +0000

    Fixed breakage of bdist_* commands that call the 'install' command.
    --HG--
    branch : setuptools
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/sandbox/trunk/setuptools%4041190

--- setuptools/command/install.py
+++ setuptools/command/install.py
@@ -1,8 +1,8 @@
-import setuptools
+import setuptools, sys
 from distutils.command.install import install as _install
 
 class install(_install):
-    """Build dependencies before installation"""
+    """Use easy_install to install the package, w/dependencies"""
 
     def handle_extra_path(self):
         # We always ignore extra_path, because we always install eggs
@@ -11,6 +11,16 @@ class install(_install):
         self.extra_dirs = ''
 
     def run(self):
+        calling_module = sys._getframe(1).f_globals.get('__name__','')
+        if calling_module != 'distutils.dist':
+            # We're not being run from the command line, so use old-style
+            # behavior.  This is a bit kludgy, because a command might use
+            # dist.run_command() to run 'install', but bdist_dumb and
+            # bdist_wininst both call run directly at the moment.
+            # When this is part of the distutils, the old install behavior
+            # should probably be requested with a flag, or a different method.
+            return _install.run(self)
+
         from setuptools.command.easy_install import easy_install
         cmd = easy_install(
             self.distribution, args="x", ignore_conflicts_at_my_risk=1
@@ -27,5 +37,4 @@ class install(_install):
         cmd.args = args
         cmd.run()
         setuptools.bootstrap_install_from = None
-
 
