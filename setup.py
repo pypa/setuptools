8f158d47596b45487e91a9c1869fb965b8c90d74
PJ Eby <distutils-sig@python.org>
2005-10-17 02:44:39 +0000

    Prep for 0.6a6 release.
    --HG--
    branch : setuptools
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/sandbox/trunk/setuptools%4041264

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_description():
     f.close()
     return ''.join(lines)
 
-VERSION = "0.6a5"
+VERSION = "0.6a6"
 from setuptools import setup, find_packages
 import sys
 from setuptools.command import __all__ as SETUP_COMMANDS
