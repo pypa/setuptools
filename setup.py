31a50d65fca476cdccdb4c9eea645aa5680ec71c
PJ Eby <distutils-sig@python.org>
2005-11-03 02:34:26 +0000

    Switch setuptools to use 'dev-rNNNN' version tags by default, and configure so that people can use 'ez_setup.py setuptools==dev' to fetch the latest in-development version.
    --HG--
    branch : setuptools
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/sandbox/trunk/setuptools%4041381

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_description():
     f.close()
     return ''.join(lines)
 
-VERSION = "0.6a7"
+VERSION = "0.6a8"
 from setuptools import setup, find_packages
 import sys
 from setuptools.command import __all__ as SETUP_COMMANDS
