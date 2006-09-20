7e0489e508d6f3edeb525c05f1c84f19b94361a6
PJ Eby <distutils-sig@python.org>
2006-09-20 20:22:04 +0000

    Backport some Python 2.5 compatibility work
    --HG--
    branch : setuptools-0.6
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/sandbox/branches/setuptools-0.6%4051932

--- setuptools/tests/doctest.py
+++ setuptools/tests/doctest.py
@@ -1330,11 +1330,13 @@ class DocTestRunner:
     __LINECACHE_FILENAME_RE = re.compile(r'<doctest '
                                          r'(?P<name>[\w\.]+)'
                                          r'\[(?P<examplenum>\d+)\]>$')
-    def __patched_linecache_getlines(self, filename):
+    def __patched_linecache_getlines(self, filename, module_globals=None):
         m = self.__LINECACHE_FILENAME_RE.match(filename)
         if m and m.group('name') == self.test.name:
             example = self.test.examples[int(m.group('examplenum'))]
             return example.source.splitlines(True)
+        elif self.save_linecache_getlines.func_code.co_argcount>1:
+            return self.save_linecache_getlines(filename, module_globals)
         else:
             return self.save_linecache_getlines(filename)
 
