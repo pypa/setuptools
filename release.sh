70c294c9d39d953dd1f64f52e753a82a5e206bbe
PJ Eby <distutils-sig@python.org>
2006-09-21 22:11:22 +0000

    Build & upload win32 installers
    --HG--
    branch : setuptools-0.6
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/sandbox/branches/setuptools-0.6%4051962

--- release.sh
+++ release.sh
@@ -9,9 +9,9 @@
 
 export VERSION="0.6c4"
 
-python2.3 setup.py -q release source && \
-python2.4 setup.py -q release binary && \
-python2.5 setup.py -q release binary && \
+python2.3 setup.py -q release source --target-version=2.3 upload && \
+python2.4 setup.py -q release binary --target-version=2.4 upload && \
+python2.5 setup.py -q release binary --target-version=2.5 upload && \
 python2.3 ez_setup.py --md5update dist/setuptools-$VERSION*-py2.?.egg && \
   cp ez_setup.py virtual-python.py ~/distrib/ && \
   cp ez_setup.py ~/projects/ez_setup/__init__.py && \
