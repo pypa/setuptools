0660a02f3cce2010406b4f2ec597142430e4265d
PJ Eby <distutils-sig@python.org>
2006-09-21 22:01:22 +0000

    Fix ``upload`` not uploading files built by ``bdist_rpm`` on Python 2.3 and 2.4.  (Backport from trunk)
    --HG--
    branch : setuptools-0.6
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/sandbox/branches/setuptools-0.6%4051961

--- setuptools/command/bdist_rpm.py
+++ setuptools/command/bdist_rpm.py
@@ -1,8 +1,10 @@
 # This is just a kludge so that bdist_rpm doesn't guess wrong about the
 # distribution name and version, if the egg_info command is going to alter
-# them, and another kludge to allow you to build old-style non-egg RPMs
+# them, another kludge to allow you to build old-style non-egg RPMs, and
+# finally, a kludge to track .rpm files for uploading when run on Python <2.5.
 
 from distutils.command.bdist_rpm import bdist_rpm as _bdist_rpm
+import sys
 
 class bdist_rpm(_bdist_rpm):
 
@@ -10,9 +12,32 @@ class bdist_rpm(_bdist_rpm):
         _bdist_rpm.initialize_options(self)
         self.no_egg = None
 
+    if sys.version<"2.5":
+        # Track for uploading any .rpm file(s) moved to self.dist_dir
+        def move_file(self, src, dst, level=1):
+            _bdist_rpm.move_file(self, src, dst, level)
+            if dst==self.dist_dir and src.endswith('.rpm'):
+                getattr(self.distribution,'dist_files',[]).append(
+                    ('bdist_egg',
+                    src.endswith('.src.rpm') and 'any' or get_python_version(),
+                     os.path.join(dst, os.path.basename(src)))
+                )
+
     def run(self):
         self.run_command('egg_info')    # ensure distro name is up-to-date
         _bdist_rpm.run(self)
+
+
+
+
+
+
+
+
+
+
+
+
 
     def _make_spec_file(self):
         version = self.distribution.get_version()
@@ -35,17 +60,6 @@ class bdist_rpm(_bdist_rpm):
         ]
         spec.insert(spec.index(line24)+1, "%define unmangled_version "+version)
         return spec
-
-
-
-
-
-
-
-
-
-
-
 
 
 
