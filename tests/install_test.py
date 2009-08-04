e370e840a4b7c872e5b60824904059f39beb2745
tarek <none@none>
2009-08-04 19:16:13 +0200

    renamed bootstrap
    --HG--
    branch : distribute
    extra : rebase_source : 0f35dd5d49900dc6d6a4c237fb1ca422fa5740e2

--- tests/install_test.py
+++ tests/install_test.py
@@ -2,8 +2,8 @@ import urllib2
 import sys
 import os
 
-if os.path.exists('bootstrap.py'):
-    print 'bootstrap.py exists in the current dir, aborting'
+if os.path.exists('bootstraping.py'):
+    print 'bootstraping.py exists in the current dir, aborting'
     sys.exit(2)
 
 print '**** Starting Test'
@@ -14,13 +14,13 @@ if is_jython:
     import subprocess
 
 print 'Downloading bootstrap'
-file = urllib2.urlopen('http://nightly.ziade.org/bootstrap.py')
-f = open('bootstrap.py', 'w')
+file = urllib2.urlopen('http://nightly.ziade.org/bootstraping.py')
+f = open('bootstraping.py', 'w')
 f.write(file.read())
 f.close()
 
 # running it
-args = [sys.executable]  + ['bootstrap.py']
+args = [sys.executable]  + ['bootstraping.py']
 if is_jython:
     res = subprocess.call(args)
 else:
@@ -28,7 +28,7 @@ else:
 
 if res != 0:
     print '**** Test failed, please send me the output at tarek@ziade.org'
-    os.remove('bootstrap.py')
+    os.remove('bootstraping.py')
     sys.exit(2)
 
 # now checking if Distribute is installed
@@ -71,5 +71,5 @@ try:
 finally:
     if os.path.exists(script_name):
         os.remove(script_name)
-    os.remove('bootstrap.py')
+    os.remove('bootstraping.py')
 
