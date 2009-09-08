7a5b40fd7a6ffc4edff0d3aafacab5dad57384e7
tarek <none@none>
2009-09-08 22:53:10 +0200

    added the scp to the release process
    --HG--
    branch : distribute
    extra : rebase_source : b2c00661e65ae5afb94832143b2b27cdd7b20949

--- release.sh
+++ release.sh
@@ -7,3 +7,6 @@ rm -rf dist
 # now preparing the source release
 python2.6 setup.py -q egg_info -RDb '' sdist register upload
 
+# pushin the bootstrap script
+scp distribute_setup.py ziade.org:nightly/build/
+
