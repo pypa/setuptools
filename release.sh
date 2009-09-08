55c87e48495b5e05c4f3e985354159ea55b8d73f
tarek <none@none>
2009-09-08 15:37:41 +0200

    the md5 hashes are made before the eggs are built
    --HG--
    branch : distribute
    extra : rebase_source : e4c1f1420cf24aa2a8b12304b86f1b3d8c6a1aaf

--- release.sh
+++ release.sh
@@ -5,17 +5,17 @@ export VERSION="0.6.1"
 # creating the releases
 rm -rf dist
 
-# eggs
-python2.3 setup.py -q egg_info -RDb '' bdist_egg # manual upload
-python2.4 setup.py -q egg_info -RDb '' bdist_egg # manual upload
-python2.5 setup.py -q egg_info -RDb '' bdist_egg register upload
-python2.6 setup.py -q egg_info -RDb '' bdist_egg register upload
-
 # updating the md5 hashes
 python distribute_setup.py --md5update dist/distribute-$VERSION-py2.3.egg
 python distribute_setup.py --md5update dist/distribute-$VERSION-py2.4.egg
 python distribute_setup.py --md5update dist/distribute-$VERSION-py2.5.egg
 python distribute_setup.py --md5update dist/distribute-$VERSION-py2.6.egg
+
+# eggs
+python2.3 setup.py -q egg_info -RDb '' bdist_egg # manual upload
+python2.4 setup.py -q egg_info -RDb '' bdist_egg # manual upload
+python2.5 setup.py -q egg_info -RDb '' bdist_egg register upload
+python2.6 setup.py -q egg_info -RDb '' bdist_egg register upload
 
 # now preparing the source release
 python2.6 setup.py -q egg_info -RDb '' sdist register upload
