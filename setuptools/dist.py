15a0f215d79be0bd7cbf521395377fd7c3168dfd
Martin v. Löwis <martin@v.loewis.de>
2009-09-13 14:40:15 +0200

    Deal with convert_doctests_2to3 not being set.
    --HG--
    branch : distribute
    extra : rebase_source : c1952763f89af6f9c5e25e67982b96756d7336e3

--- setuptools/dist.py
+++ setuptools/dist.py
@@ -255,7 +255,7 @@ class Distribution(_Distribution):
             if value is not None:
                 ep.require(installer=self.fetch_build_egg)
                 ep.load()(self, ep.name, value)
-        if self.convert_doctests_2to3:
+        if getattr(self, 'convert_doctests_2to3', None):
             # XXX may convert to set here when we can rely on set being builtin
             self.convert_doctests_2to3 = [os.path.abspath(p) for p in self.convert_doctests_2to3]
         else:
