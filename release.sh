79f61d0c5d40e0d1e100d6f6754c9b1b4c003829
tarek <none@none>
2009-10-09 12:54:49 +0200

    using a temp dir to avoid using the site.py that is in the current dir when 2to3 runs
    --HG--
    branch : distribute
    extra : rebase_source : 18f1e97291dc5745849df981627630295acd601e

--- release.sh
+++ release.sh
@@ -2,17 +2,21 @@
 export VERSION="0.6.4"
 
 # creating the 3k script
-cp distribute_setup.py distribute_setup.py.back 
-2to3 -w distribute_setup.py > /dev/null 
-mv distribute_setup.py distribute_setup_3k.py 
-mv distribute_setup.py.back distribute_setup.py
+mkdir ./temp
+cp distribute_setup.py ./temp/distribute_setup.py
+cd ./temp
+2to3 -w distribute_setup.py > /dev/null
+mv distribute_setup.py ../distribute_setup_3k.py
+cd ..
+rm -rf ./temp
 
 # creating the releases
-rm -rf dist
+rm -rf ./dist
 
 # now preparing the source release
 python2.6 setup.py -q egg_info -RDb '' sdist register upload
 
-# pushing the bootstrap script
+# pushing the bootstrap scripts
 scp distribute_setup.py ziade.org:nightly/build/
+scp distribute_setup_3k.py ziade.org:nightly/build/
 
