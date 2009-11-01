7405419fb048f82ef142afd32d83dcb29bab59d4
tarek <none@none>
2009-11-01 15:15:49 +0100

    raised version in distribute_setup as well
    --HG--
    branch : distribute
    extra : rebase_source : 660ea268a383973b586ca0889c2ca8f9c87d36e8

--- distribute_setup.py
+++ distribute_setup.py
@@ -46,7 +46,7 @@ except ImportError:
             args = [quote(arg) for arg in args]
         return os.spawnl(os.P_WAIT, sys.executable, *args) == 0
 
-DEFAULT_VERSION = "0.6.6"
+DEFAULT_VERSION = "0.6.8"
 DEFAULT_URL = "http://pypi.python.org/packages/source/d/distribute/"
 SETUPTOOLS_PKG_INFO = """\
 Metadata-Version: 1.0
@@ -113,7 +113,7 @@ def _build_egg(egg, tarball, to_dir):
 
 
 def _do_download(version, download_base, to_dir, download_delay):
-    egg = os.path.join(to_dir, 'distribute-%s-py%d.%d.egg' 
+    egg = os.path.join(to_dir, 'distribute-%s-py%d.%d.egg'
                        % (version, sys.version_info[0], sys.version_info[1]))
     if not os.path.exists(egg):
         tarball = download_setuptools(version, download_base,
