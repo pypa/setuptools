b9554b5bc4ca05034d3b9dce876b684fd0482c4a
tarek <none@none>
2009-12-10 16:20:18 +0100

    Uses plistlib instead of re. Thanks Florian refs #92
    --HG--
    branch : distribute
    extra : rebase_source : f3e61a21dc640a621364736b2a60fa9eb337ae1b

--- pkg_resources.py
+++ pkg_resources.py
@@ -188,14 +188,13 @@ def _macosx_vers(_cache=[]):
         version = platform.mac_ver()[0]
         # fallback for MacPorts
         if version == '':
-            import re
-            version_file = '/System/Library/CoreServices/SystemVersion.plist'
-            version_regexp = r'<key>ProductVersion</key>\n\t<string>(.*?)</string>'
-            if os.path.exists(version_file):
-                osx_version = open(version_file).read()
-                osx_version = re.findall(version_regexp, osx_version, re.M)
-                if len(osx_version) > 0:
-                    version = osx_version[0]
+            import plistlib
+            plist = '/System/Library/CoreServices/SystemVersion.plist'
+            if os.path.exists(plist):
+                plist_content = plistlib.readPlist(plist)
+                if 'ProductVersion' in plist_content:
+                    version = plist_content['ProductVersion']
+
         _cache.append(version.split('.'))
     return _cache[0]
 
