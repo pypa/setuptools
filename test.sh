24773fa31a13fc0058aed577d566cb528a769061
Tarek Ziade <tarek@ziade.org>
2010-05-19 11:49:00 +0200

    added the -q flag and make sure the build folder is removed when python 3 tests are run
    --HG--
    branch : distribute
    extra : rebase_source : 5ebb6fedf532a2a517cbcf522cc9ec3a9066ea0a

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
-python2.3 setup.py test
-python2.4 setup.py test
-python2.5 setup.py test
-python2.6 setup.py test
-python3.1 setup.py test
+python2.3 setup.py -q test
+python2.4 setup.py -q test
+python2.5 setup.py -q test
+python2.6 setup.py -q test
+rm -rf build
+python3.1 setup.py -q test
 
