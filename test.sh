e1ad7a3ac2ee213f7ce9397fa68b85962b38f29c
Tarek Ziade <tarek@ziade.org>
2010-05-25 22:36:37 +0200

    make sure tests.sh return the exit code, and nicer outputs
    --HG--
    branch : distribute
    extra : rebase_source : 6e614dc25657d2b5ccabbf4ac48c778e712cff30

--- test.sh
+++ test.sh
@@ -1,9 +1,47 @@
-python2.3 setup.py -q test
-python2.4 setup.py -q test
-python2.5 setup.py -q test
-python2.6 setup.py -q test
-python2.7 setup.py -q test
+#!/bin/sh
+echo -n "Running tests for Python 2.3..."
+python2.3 setup.py -q test > /dev/null 2> /dev/null
+if [ $? -ne 0 ];then
+    echo "Failed"
+    exit $1
+else
+    echo "Success"
+fi
+
+echo -n "Running tests for Python 2.4..."
+python2.4 setup.py -q test > /dev/null 2> /dev/null
+if [ $? -ne 0 ];then
+    echo "Failed"
+    exit $1
+else
+    echo "Success"
+fi
+
+echo -n "Running tests for Python 2.5..."
+python2.5 setup.py -q test > /dev/null 2> /dev/null
+if [ $? -ne 0 ];then
+    echo "Failed"
+    exit $1
+else
+    echo "Success"
+fi
+
+echo -n "Running tests for Python 2.6..."
+python2.6 setup.py -q test > /dev/null 2> /dev/null
+if [ $? -ne 0 ];then
+    echo "Failed"
+    exit $1
+else
+    echo "Success"
+fi
 
 rm -rf build
-python3.1 setup.py -q test
+echo -n "Running tests for Python 3.1..."
+python3.1 setup.py -q test > /dev/null 2> /dev/null
+if [ $? -ne 0 ];then
+    echo "Failed"
+    exit $1
+else
+    echo "Success"
+fi
 
