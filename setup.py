c4318e2572b0158aa05fa241b85552073bb8400b
Tarek Ziade <tarek@ziade.org>
2010-05-31 19:39:15 +0200

    starting 0.6.14
    --HG--
    branch : distribute
    extra : rebase_source : b9e623ce0a5b70680f18c48e1c5f64753b70d38c

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ init_path = convert_path('setuptools/command/__init__.py')
 exec(open(init_path).read(), d)
 
 SETUP_COMMANDS = d['__all__']
-VERSION = "0.6.13"
+VERSION = "0.6.14"
 
 from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py as _build_py
