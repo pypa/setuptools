ed9ebca5b0c012c6b44adad4efa475f1cccd460d
Jason R. Coombs <jaraco@jaraco.com>
2012-03-10 19:03:23 -0800

    Indent with spaces
    --HG--
    branch : distribute
    extra : rebase_source : 799441b639f6f9c55fe25745e793ac29419f0293

--- setuptools/extension.py
+++ setuptools/extension.py
@@ -5,12 +5,12 @@ _Extension = _get_unpatched(_Extension)
 # Prefer Cython to Pyrex
 pyrex_impls = 'Cython.Distutils.build_ext', 'Pyrex.Distutils.build_ext'
 for pyrex_impl in pyrex_impls:
-	try:
-		# from (pyrex_impl) import build_ext
-		build_ext = __import__(pyrex_impl, fromlist=['build_ext']).build_ext
-		break
-	except:
-		pass
+    try:
+        # from (pyrex_impl) import build_ext
+        build_ext = __import__(pyrex_impl, fromlist=['build_ext']).build_ext
+        break
+    except:
+        pass
 have_pyrex = 'build_ext' in globals()
 
 
@@ -18,7 +18,7 @@ class Extension(_Extension):
     """Extension that uses '.c' files in place of '.pyx' files"""
 
     if not have_pyrex:
-        # convert .pyx extensions to .c 
+        # convert .pyx extensions to .c
         def __init__(self,*args,**kw):
             _Extension.__init__(self,*args,**kw)
             sources = []
