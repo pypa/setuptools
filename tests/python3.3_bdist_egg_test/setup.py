311fb781f87f8885d9a94343b4b7e9731bc0c9e7
Marc Abramowitz <marc@marc-abramowitz.com>
2012-06-26 15:15:27 -0700

    Add tests and fix for marshal.load of pyc files on Python 3.3
    Fixes
    https://bitbucket.org/tarek/distribute/issue/283/bdist_egg-issues-with-python-330ax
    
    --HG--
    branch : distribute
    extra : rebase_source : ec6f2611d3f8a54b7e11cfadf9d03fdcdef39639

---
+++ tests/python3.3_bdist_egg_test/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python3.3_bdist_egg_test',
+    version='0.0.0',
+    description='Test',
+    license='BSD',
+    py_modules=['module'],
+    author='Marc Abramowitz',
+    author_email='marc@marc-abramowitz.com',
+    url='https://bitbucket.org/msabramo/python3.3_bdist_egg_test',
+    # zip_safe=False,
+)
