0a5180da06efb1d3e854cbdfec458968790c3a5e
Marc Abramowitz <marc@marc-abramowitz.com>
2012-07-07 12:29:07 -0700

    Remove unnecessary stuff from test_python33_bdist_egg.py
    --HG--
    branch : distribute
    extra : rebase_source : 3a993d4c266798501557f338abc8c4cdc2db3c8c

--- tests/test_python33_bdist_egg.py
+++ tests/test_python33_bdist_egg.py
@@ -1,18 +1,12 @@
 import sys
 import os
-import tempfile
 import unittest
-import shutil
-import copy
 
 CURDIR = os.path.abspath(os.path.dirname(__file__))
 TOPDIR = os.path.split(CURDIR)[0]
 sys.path.insert(0, TOPDIR)
 
-from distribute_setup import (use_setuptools, _build_egg, _python_cmd,
-                              _do_download, _install, DEFAULT_URL,
-                              DEFAULT_VERSION)
-import distribute_setup
+from distribute_setup import _python_cmd
 
 class TestPython33BdistEgg(unittest.TestCase):
 
