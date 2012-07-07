115c62e6a9a9167633eff93ef0f1a355505a0e5d
Marc Abramowitz <marc@marc-abramowitz.com>
2012-07-07 11:51:13 -0700

    Add a test for Python 3.3 bdist_egg issue
    --HG--
    branch : distribute
    extra : rebase_source : e83ee69c3b15e4a75780811a7bb3612b8f7f54d1

---
+++ tests/test_python33_bdist_egg.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import tempfile
+import unittest
+import shutil
+import copy
+
+CURDIR = os.path.abspath(os.path.dirname(__file__))
+TOPDIR = os.path.split(CURDIR)[0]
+sys.path.insert(0, TOPDIR)
+
+from distribute_setup import (use_setuptools, _build_egg, _python_cmd,
+                              _do_download, _install, DEFAULT_URL,
+                              DEFAULT_VERSION)
+import distribute_setup
+
+class TestPython33BdistEgg(unittest.TestCase):
+
+    def test_build_egg(self):
+        os.chdir(os.path.join(CURDIR, 'python3.3_bdist_egg_test'))
+        _python_cmd("setup.py", "bdist_egg")
+
+
+if __name__ == '__main__':
+    unittest.main()
