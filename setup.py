08b3db92e72b9ab3d91c5cf604b4ef734d256da9
Jason R. Coombs <jaraco@jaraco.com>
2012-07-22 15:06:00 -0400

    Updated Trove classifiers to indicate supported Python versions
    --HG--
    branch : distribute
    extra : rebase_source : 430e99bf42cb46f6ac996d26d5b2fd1f0000418e

--- setup.py
+++ setup.py
@@ -207,7 +207,10 @@ dist = setup(
         License :: OSI Approved :: Python Software Foundation License
         License :: OSI Approved :: Zope Public License
         Operating System :: OS Independent
-        Programming Language :: Python
+        Programming Language :: Python :: 2.4
+        Programming Language :: Python :: 2.5
+        Programming Language :: Python :: 2.6
+        Programming Language :: Python :: 2.7
         Programming Language :: Python :: 3
         Topic :: Software Development :: Libraries :: Python Modules
         Topic :: System :: Archiving :: Packaging
