a4b326909d1f64037412b5048d59199bc6b648e8
Lennart Regebro <regebro@gmail.com>
2012-08-20 19:28:30 +0200

    I guess nobody uses test.sh anymore, since it's unmaintained. Updated anyway.
    --HG--
    branch : distribute
    extra : rebase_source : d4105c985efec3f6d7e298615299c5cc9359cbc2

--- test.sh
+++ test.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-echo -n "Running tests for Python 2.3..."
-python2.3 setup.py -q test > /dev/null 2> /dev/null
-if [ $? -ne 0 ];then
-    echo "Failed"
-    exit $1
-else
-    echo "Success"
-fi
-
 echo -n "Running tests for Python 2.4..."
 python2.4 setup.py -q test > /dev/null 2> /dev/null
 if [ $? -ne 0 ];then
@@ -38,6 +29,16 @@ fi
 rm -rf build
 echo -n "Running tests for Python 3.1..."
 python3.1 setup.py -q test > /dev/null 2> /dev/null
+if [ $? -ne 0 ];then
+    echo "Failed"
+    exit $1
+else
+    echo "Success"
+fi
+
+rm -rf build
+echo -n "Running tests for Python 3.2..."
+python3.2 setup.py -q test > /dev/null 2> /dev/null
 if [ $? -ne 0 ];then
     echo "Failed"
     exit $1
