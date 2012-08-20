ce53222d269dbf6bed8d97ed01437aba89faba53
Lennart Regebro <regebro@gmail.com>
2012-08-20 19:35:57 +0200

    And Python 2.7, of course.
    --HG--
    branch : distribute
    extra : rebase_source : 81e1d3c6e30c8096505bfdfd25bfe49b5dc37953

--- test.sh
+++ test.sh
@@ -26,6 +26,15 @@ else
     echo "Success"
 fi
 
+echo -n "Running tests for Python 2.7..."
+python2.7 setup.py -q test > /dev/null 2> /dev/null
+if [ $? -ne 0 ];then
+    echo "Failed"
+    exit $1
+else
+    echo "Success"
+fi
+
 rm -rf build
 echo -n "Running tests for Python 3.1..."
 python3.1 setup.py -q test > /dev/null 2> /dev/null
