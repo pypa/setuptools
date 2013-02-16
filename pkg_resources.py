b5cd47fbd96ae900dd835b68484084ab1219f260
Jason R. Coombs <jaraco@jaraco.com>
2013-02-16 02:13:45 -0500

    Backed out changeset: d0a8d1a83053 In the discussion in #278, it became clear that the deviance in behavior from setuptools is problemmatic. For compatibility, defer to the setuptools version scheme as specifically intended.
    --HG--
    branch : distribute
    extra : rebase_source : 1d5cc8c216f974b247e2dc84d8d40c868d6d3639

--- pkg_resources.py
+++ pkg_resources.py
@@ -1936,7 +1936,7 @@ replace = {'pre':'c', 'preview':'c','-':'final-','rc':'c','dev':'@'}.get
 def _parse_version_parts(s):
     for part in component_re.split(s):
         part = replace(part,part)
-        if part in ['', '.']:
+        if not part or part=='.':
             continue
         if part[:1] in '0123456789':
             yield part.zfill(8)    # pad for numeric comparison
@@ -1979,6 +1979,8 @@ def parse_version(s):
     parts = []
     for part in _parse_version_parts(s.lower()):
         if part.startswith('*'):
+            if part<'*final':   # remove '-' before a prerelease tag
+                while parts and parts[-1]=='*final-': parts.pop()
             # remove trailing zeros from each series of numeric parts
             while parts and parts[-1]=='00000000':
                 parts.pop()
