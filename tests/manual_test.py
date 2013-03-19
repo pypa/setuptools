06b90a07da2bc7fded73743ca5bbf38200e972d8
Jason R. Coombs <jaraco@jaraco.com>
2013-03-19 12:01:43 -0700

    Updated manual test to test against setuptools
    --HG--
    branch : Setuptools-Distribute merge
    extra : rebase_source : ff968de5ea0e28052c0b53fecdcf0c348a6faf04

--- tests/manual_test.py
+++ tests/manual_test.py
@@ -51,9 +51,8 @@ eggs =
     extensions
 """
 
-BOOTSTRAP = 'http://python-distribute.org/bootstrap.py'
+BOOTSTRAP = 'http://downloads.buildout.org/1/bootstrap.py'
 PYVER = sys.version.split()[0][:3]
-DEV_URL = 'http://bitbucket.org/tarek/distribute/get/0.6-maintenance.zip#egg=distribute-dev'
 
 _VARS = {'base': '.',
          'py_version_short': PYVER}
@@ -66,26 +65,25 @@ else:
 
 @tempdir
 def test_virtualenv():
-    """virtualenv with distribute"""
+    """virtualenv with setuptools"""
     purelib = os.path.abspath(Template(PURELIB).substitute(**_VARS))
-    _system_call('virtualenv', '--no-site-packages', '.', '--distribute')
-    _system_call('bin/easy_install', 'distribute==dev')
+    _system_call('virtualenv', '--no-site-packages', '.')
+    _system_call('bin/easy_install', 'setuptools==dev')
     # linux specific
     site_pkg = os.listdir(purelib)
     site_pkg.sort()
-    assert 'distribute' in site_pkg[0]
+    assert 'setuptools' in site_pkg[0]
     easy_install = os.path.join(purelib, 'easy-install.pth')
     with open(easy_install) as f:
         res = f.read()
-    assert 'distribute' in res
-    assert 'setuptools' not in res
+    assert 'setuptools' in res
 
 @tempdir
 def test_full():
     """virtualenv + pip + buildout"""
     _system_call('virtualenv', '--no-site-packages', '.')
-    _system_call('bin/easy_install', '-q', 'distribute==dev')
-    _system_call('bin/easy_install', '-qU', 'distribute==dev')
+    _system_call('bin/easy_install', '-q', 'setuptools==dev')
+    _system_call('bin/easy_install', '-qU', 'setuptools==dev')
     _system_call('bin/easy_install', '-q', 'pip')
     _system_call('bin/pip', 'install', '-q', 'zc.buildout')
 
@@ -95,16 +93,16 @@ def test_full():
     with open('bootstrap.py', 'w') as f:
         f.write(urlopen(BOOTSTRAP).read())
 
-    _system_call('bin/python', 'bootstrap.py', '--distribute')
+    _system_call('bin/python', 'bootstrap.py')
     _system_call('bin/buildout', '-q')
     eggs = os.listdir('eggs')
     eggs.sort()
     assert len(eggs) == 3
-    assert eggs[0].startswith('distribute')
-    assert eggs[1:] == ['extensions-0.3-py2.6.egg',
-                        'zc.recipe.egg-1.2.2-py2.6.egg']
+    assert eggs[1].startswith('setuptools')
+    del eggs[1]
+    assert eggs == ['extensions-0.3-py2.6.egg',
+        'zc.recipe.egg-1.2.2-py2.6.egg']
 
 if __name__ == '__main__':
     test_virtualenv()
     test_full()
-
