8cbf071653bf990df550d6fa9b7b0b10f23143aa
Jason R. Coombs <jaraco@jaraco.com>
2013-07-15 13:34:57 -0400

    Bumped to 0.9.4 in preparation for next release.

--- setuptools/__init__.py
+++ setuptools/__init__.py
@@ -8,7 +8,7 @@ from distutils.util import convert_path
 import os
 import sys
 
-__version__ = '0.9.3'
+__version__ = '0.9.4'
 __all__ = [
     'setup', 'Distribution', 'Feature', 'Command', 'Extension', 'Require',
     'find_packages'
