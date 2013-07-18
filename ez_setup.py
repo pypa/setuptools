d748f5e7c9107123704a0907bdb87a2eb5e149a8
Jason R. Coombs <jaraco@jaraco.com>
2013-07-17 21:08:57 -0400

    Bumped to 0.9.7 in preparation for next release.

--- ez_setup.py
+++ ez_setup.py
@@ -28,7 +28,7 @@ try:
 except ImportError:
     USER_SITE = None
 
-DEFAULT_VERSION = "0.9.6"
+DEFAULT_VERSION = "0.9.7"
 DEFAULT_URL = "https://pypi.python.org/packages/source/s/setuptools/"
 
 def _python_cmd(*args):
