621f405d07fc9f3509a186c7650c6752b0436e01
Niklas Rigemo <niklas.rigemo@gmail.com>
2013-09-19 12:48:10 +0200

    PEP8: Corrected multiple statements on one line (semicolon)
    --HG--
    branch : patch1

--- setuptools/script template (dev).py
+++ setuptools/script template (dev).py
@@ -1,6 +1,7 @@
 # EASY-INSTALL-DEV-SCRIPT: %(spec)r,%(script_name)r
 __requires__ = """%(spec)r"""
-from pkg_resources import require; require("""%(spec)r""")
+from pkg_resources import require
+require("""%(spec)r""")
 del require
 __file__ = """%(dev_path)r"""
 try:
