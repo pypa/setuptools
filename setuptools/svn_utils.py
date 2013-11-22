cc90c5093d272397f08e33e4e75bf762d419df27
philip_thiem <ptthiem+bitbuket@gmail.com>
2013-11-22 00:51:45 +0000

    extraneous comment left in

--- setuptools/svn_utils.py
+++ setuptools/svn_utils.py
@@ -90,12 +90,11 @@ def determine_console_encoding():
     except (locale.Error, LookupError):
         encoding = None
         
-    #olders pythons defaulted to this
     is_osx = sys.platform == "darwin"
     if not encoding:
         return ["US-ASCII", "utf-8"][is_osx]
     elif encoding.startswith("mac-") and is_osx:
-        #certain version of pythons would return mac-roman as default
+        #certain versions of python would return mac-roman as default
         #OSX as a left over of earlier mac versions.
         return "utf-8"
     else:
