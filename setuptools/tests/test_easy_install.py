4ba80b5874929c4408f0af68a6ff2a152c03c314
Jason R. Coombs <jaraco@jaraco.com>
2013-11-27 14:49:22 -0500

    Remove unused variable and clean whitespace

--- setuptools/tests/test_easy_install.py
+++ setuptools/tests/test_easy_install.py
@@ -16,7 +16,7 @@ import distutils.core
 from setuptools.compat import StringIO, BytesIO, next, urlparse
 from setuptools.sandbox import run_setup, SandboxViolation
 from setuptools.command.easy_install import easy_install, fix_jython_executable, get_script_args, nt_quote_arg
-from setuptools.command.easy_install import  PthDistributions
+from setuptools.command.easy_install import PthDistributions
 from setuptools.command import easy_install as easy_install_pkg
 from setuptools.dist import Distribution
 from pkg_resources import Distribution as PRDistribution
@@ -243,7 +243,6 @@ class TestUserInstallTest(unittest.TestCase):
 
         test_pkg = os.path.join(self.dir, 'test_pkg')
         test_setup_py = os.path.join(test_pkg, 'setup.py')
-        test_setup_cfg = os.path.join(test_pkg, 'setup.cfg')
         os.mkdir(test_pkg)
 
         f = open(test_setup_py, 'w')
