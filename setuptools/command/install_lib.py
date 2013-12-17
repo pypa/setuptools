646d7585da63b3fe5b4b2d5bf057b3795ac21241
William Grzybowski <wg@FreeBSD.org>
2013-12-16 22:44:35 -0200

    Do not override _bytecode_filenames
    The overridden version cannot handle Python 3.x while distutils verion can
    handle it just fine.
    
    --HG--
    extra : rebase_source : 86fa56285849e97780e91eff405881bfb72184d5

--- setuptools/command/install_lib.py
+++ setuptools/command/install_lib.py
@@ -4,18 +4,6 @@ import os
 class install_lib(_install_lib):
     """Don't add compiled flags to filenames of non-Python files"""
 
-    def _bytecode_filenames (self, py_filenames):
-        bytecode_files = []
-        for py_file in py_filenames:
-            if not py_file.endswith('.py'):
-                continue
-            if self.compile:
-                bytecode_files.append(py_file + "c")
-            if self.optimize > 0:
-                bytecode_files.append(py_file + "o")
-
-        return bytecode_files
-
     def run(self):
         self.build()
         outfiles = self.install()
