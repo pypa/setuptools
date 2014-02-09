875393f610a867ae7258d673c1cf5638e253eff4
Jason R. Coombs <jaraco@jaraco.com>
2014-02-09 15:51:20 -0500

    Declare support for Python 3.4

--- setup.py
+++ setup.py
@@ -181,6 +181,7 @@ setup_params = dict(
         Programming Language :: Python :: 3.1
         Programming Language :: Python :: 3.2
         Programming Language :: Python :: 3.3
+        Programming Language :: Python :: 3.4
         Topic :: Software Development :: Libraries :: Python Modules
         Topic :: System :: Archiving :: Packaging
         Topic :: System :: Systems Administration
