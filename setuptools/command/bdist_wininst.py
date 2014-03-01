5ae17b58d4823cb5e74ec1b7802c0debbc119dc9
Jason R. Coombs <jaraco@jaraco.com>
2014-03-01 15:55:45 -0500

    Remove patching of upload_names (dist_files) - Python 2.6 already does the right thing.

--- setuptools/command/bdist_wininst.py
+++ setuptools/command/bdist_wininst.py
@@ -1,27 +1,6 @@
 from distutils.command.bdist_wininst import bdist_wininst as _bdist_wininst
 
 class bdist_wininst(_bdist_wininst):
-    _good_upload = _bad_upload = None
-
-    def create_exe(self, arcname, fullname, bitmap=None):
-        _bdist_wininst.create_exe(self, arcname, fullname, bitmap)
-        installer_name = self.get_installer_filename(fullname)
-        if self.target_version:
-            pyversion = self.target_version
-            # fix 2.5+ bdist_wininst ignoring --target-version spec
-            self._bad_upload = ('bdist_wininst', 'any', installer_name)
-        else:
-            pyversion = 'any'
-        self._good_upload = ('bdist_wininst', pyversion, installer_name)
-
-    def _fix_upload_names(self):
-        good, bad = self._good_upload, self._bad_upload
-        dist_files = getattr(self.distribution, 'dist_files', [])
-        if bad in dist_files:
-            dist_files.remove(bad)
-        if good not in dist_files:
-            dist_files.append(good)
-
     def reinitialize_command(self, command, reinit_subcommands=0):
         cmd = self.distribution.reinitialize_command(
             command, reinit_subcommands)
@@ -33,6 +12,5 @@ class bdist_wininst(_bdist_wininst):
         self._is_running = True
         try:
             _bdist_wininst.run(self)
-            self._fix_upload_names()
         finally:
             self._is_running = False
