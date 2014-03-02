404b7af74fb65299aa9c14e0e40541e3a4a68285
Jason R. Coombs <jaraco@jaraco.com>
2014-03-01 19:32:11 -0500

    Update workaround to reference filed ticket.

--- setuptools/command/bdist_wininst.py
+++ setuptools/command/bdist_wininst.py
@@ -2,10 +2,14 @@ from distutils.command.bdist_wininst import bdist_wininst as _bdist_wininst
 
 class bdist_wininst(_bdist_wininst):
     def reinitialize_command(self, command, reinit_subcommands=0):
+        """
+        Supplement reinitialize_command to work around
+        http://bugs.python.org/issue20819
+        """
         cmd = self.distribution.reinitialize_command(
             command, reinit_subcommands)
         if command in ('install', 'install_lib'):
-            cmd.install_lib = None  # work around distutils bug
+            cmd.install_lib = None
         return cmd
 
     def run(self):
