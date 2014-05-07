2ad829949a55ebb390c5c94a5672f10f693e7eaf
Jason R. Coombs <jaraco@jaraco.com>
2014-05-07 12:08:13 -0400

    Bumped to 3.5.3 in preparation for next release.

--- ez_setup.py
+++ ez_setup.py
@@ -31,7 +31,7 @@ try:
 except ImportError:
     USER_SITE = None
 
-DEFAULT_VERSION = "3.5.2"
+DEFAULT_VERSION = "3.5.3"
 DEFAULT_URL = "https://pypi.python.org/packages/source/s/setuptools/"
 
 def _python_cmd(*args):
