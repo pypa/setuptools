63b852dff23dc86e983fceddde1463724a9d8a07
Jason R. Coombs <jaraco@jaraco.com>
2014-06-22 10:15:21 -0400

    Reindent long line

--- setuptools/command/install_egg_info.py
+++ setuptools/command/install_egg_info.py
@@ -64,7 +64,8 @@ class install_egg_info(Command):
         "import sys, types, os",
         "p = os.path.join(sys._getframe(1).f_locals['sitedir'], *%(pth)r)",
         "ie = os.path.exists(os.path.join(p,'__init__.py'))",
-        "m = not ie and sys.modules.setdefault(%(pkg)r, types.ModuleType(%(pkg)r))",
+        "m = not ie and "
+            "sys.modules.setdefault(%(pkg)r, types.ModuleType(%(pkg)r))",
         "mp = (m or []) and m.__dict__.setdefault('__path__',[])",
         "(p not in mp) and mp.append(p)",
     )
