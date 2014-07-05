e66c4d9c3aefaf98a7d8d9b19ce5948fe7840c49
Melvyn Sopacua <melvyn@magemana.nl>
2014-07-05 17:43:39 +0200

    Fix exclude list on python 3.2+
    imp.get_tag() is only available on 3.2+. Since 2<x<3.2 are EOL we shall
    not worry.
    We could implement a local get_tag(), but the point is moot: this
    compilation tactic with __pycache__ subdirs and versioned import files
    is new to python 3.x, so hasattr() is sufficient.

--- setuptools/command/install_lib.py
+++ setuptools/command/install_lib.py
@@ -1,6 +1,5 @@
 import distutils.command.install_lib as orig
-import os
-
+import os, imp
 
 class install_lib(orig.install_lib):
     """Don't add compiled flags to filenames of non-Python files"""
@@ -17,12 +16,24 @@ class install_lib(orig.install_lib):
         nsp = self.distribution.namespace_packages
         svem = (nsp and self.get_finalized_command('install')
                 .single_version_externally_managed)
+        exclude_names = ['__init__.py', '__init__.pyc', '__init__.pyo']
+        if hasattr(imp, 'get_tag') :
+            exclude_names.extend(
+                os.path.join(
+                    '__pycache__',
+                    '__init__.' + imp.get_tag() + '.pyc'
+                ),
+                os.path.join(
+                    '__pycache__',
+                    '__init__.' + imp.get_tag() + '.pyo'
+                ),
+            )
         if svem:
             for pkg in nsp:
                 parts = pkg.split('.')
                 while parts:
                     pkgdir = os.path.join(self.install_dir, *parts)
-                    for f in '__init__.py', '__init__.pyc', '__init__.pyo':
+                    for f in exclude_names :
                         exclude[os.path.join(pkgdir, f)] = 1
                     parts.pop()
         return exclude
