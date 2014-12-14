4caa4af6ae275db9a499c27b66cc636cb9c596a1
Jason R. Coombs <jaraco@jaraco.com>
2014-12-14 17:31:19 -0500

    Bumped to 8.0.3 in preparation for next release.

--- ez_setup.py
+++ ez_setup.py
@@ -36,7 +36,7 @@ try:
 except ImportError:
     USER_SITE = None
 
-DEFAULT_VERSION = "8.0.2"
+DEFAULT_VERSION = "8.0.3"
 DEFAULT_URL = "https://pypi.python.org/packages/source/s/setuptools/"
 
 def _python_cmd(*args):
