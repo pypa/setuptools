c7de48c1f42611b25b0ab776ece87763515cc120
Jason R. Coombs <jaraco@jaraco.com>
2014-12-30 12:50:39 -0500

    Update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Setuptools'
-copyright = '2009-2013, The fellowship of the packaging'
+copyright = '2009-2014, The fellowship of the packaging'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
