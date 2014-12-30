ede1df71401a16c74dea9c1962bd1085538ac3d7
Jason R. Coombs <jaraco@jaraco.com>
2014-12-30 11:22:57 -0500

    Remove changes file from long_description.

--- setup.py
+++ setup.py
@@ -90,15 +90,8 @@ class test(_test):
 
 readme_file = io.open('README.txt', encoding='utf-8')
 
-# The release script adds hyperlinks to issues,
-# but if the release script has not run, fall back to the source file
-changes_names = 'CHANGES (links).txt', 'CHANGES.txt'
-changes_fn = next(iter(filter(os.path.exists, changes_names)))
-changes_file = io.open(changes_fn, encoding='utf-8')
-
 with readme_file:
-    with changes_file:
-        long_description = readme_file.read() + '\n' + changes_file.read()
+    long_description = readme_file.read()
 
 package_data = {
         'setuptools': ['script (dev).tmpl', 'script.tmpl', 'site-patch.py']}
