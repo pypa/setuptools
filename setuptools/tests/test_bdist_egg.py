1cbdf581186e1f34f1e48673f5faf60922e66799
Jason R. Coombs <jaraco@jaraco.com>
2015-01-01 17:31:37 -0500

    Replace setup and teardown with pytest fixtures.

--- setuptools/tests/test_bdist_egg.py
+++ setuptools/tests/test_bdist_egg.py
@@ -2,10 +2,9 @@
 """
 import os
 import re
-import shutil
-import site
-import tempfile
-import unittest
+
+import pytest
+import mock
 
 from setuptools.dist import Distribution
 
@@ -18,30 +17,31 @@ from setuptools import setup
 setup(name='foo', py_modules=['hi'])
 """
 
-class TestDevelopTest(unittest.TestCase):
+@pytest.yield_fixture
+def user_override():
+    """
+    Override site.USER_BASE and site.USER_SITE with temporary directories in
+    a context.
+    """
+    with contexts.tempdir() as user_base:
+        with mock.patch('site.USER_BASE', user_base):
+            with contexts.tempdir() as user_site:
+                with mock.patch('site.USER_SITE', user_site):
+                    yield
 
-    def setUp(self):
-        self.dir = tempfile.mkdtemp()
-        self.old_cwd = os.getcwd()
-        os.chdir(self.dir)
-        with open('setup.py', 'w') as f:
-            f.write(SETUP_PY)
-        with open('hi.py', 'w') as f:
-            f.write('1\n')
-        self.old_base = site.USER_BASE
-        site.USER_BASE = tempfile.mkdtemp()
-        self.old_site = site.USER_SITE
-        site.USER_SITE = tempfile.mkdtemp()
 
-    def tearDown(self):
-        os.chdir(self.old_cwd)
-        shutil.rmtree(self.dir)
-        shutil.rmtree(site.USER_BASE)
-        shutil.rmtree(site.USER_SITE)
-        site.USER_BASE = self.old_base
-        site.USER_SITE = self.old_site
+@pytest.yield_fixture
+def setup_context(tmpdir):
+    with (tmpdir/'setup.py').open('w') as f:
+        f.write(SETUP_PY)
+    with (tmpdir/'hi.py').open('w') as f:
+        f.write('1\n')
+    with tmpdir.as_cwd():
+        yield tmpdir
 
-    def test_bdist_egg(self):
+
+class TestDevelopTest:
+    def test_bdist_egg(self, setup_context, user_override):
         dist = Distribution(dict(
             script_name='setup.py',
             script_args=['bdist_egg'],
@@ -55,8 +55,4 @@ class TestDevelopTest(unittest.TestCase):
 
         # let's see if we got our egg link at the right place
         [content] = os.listdir('dist')
-        self.assertTrue(re.match('foo-0.0.0-py[23].\d.egg$', content))
-
-def test_suite():
-    return unittest.makeSuite(TestDevelopTest)
-
+        assert re.match('foo-0.0.0-py[23].\d.egg$', content)
