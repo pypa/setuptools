c808d232427ed8072b3b46b9e1c03df9d4cf9f76
Jason R. Coombs <jaraco@jaraco.com>
2015-01-18 20:49:13 -0500

    Bumped to 12.0.4 in preparation for next release.

--- ez_setup.py
+++ ez_setup.py
@@ -36,7 +36,7 @@ try:
 except ImportError:
     USER_SITE = None
 
-DEFAULT_VERSION = "12.0.3"
+DEFAULT_VERSION = "12.0.4"
 DEFAULT_URL = "https://pypi.python.org/packages/source/s/setuptools/"
 
 def _python_cmd(*args):
