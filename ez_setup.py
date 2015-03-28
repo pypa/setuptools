9a33ea35df1a77e721ebeb1700acaa74ca28a430
Jason R. Coombs <jaraco@jaraco.com>
2015-03-28 11:07:10 -0400

    Bumped to 15.0 in preparation for next release.

--- ez_setup.py
+++ ez_setup.py
@@ -30,7 +30,7 @@ try:
 except ImportError:
     USER_SITE = None
 
-DEFAULT_VERSION = "14.3.2"
+DEFAULT_VERSION = "15.0"
 DEFAULT_URL = "https://pypi.python.org/packages/source/s/setuptools/"
 DEFAULT_SAVE_DIR = os.curdir
 
