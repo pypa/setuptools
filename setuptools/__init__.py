af9943a4906a808c8d7c2b5004ba48f32400f1d3
Jason R. Coombs <jaraco@jaraco.com>
2015-08-30 11:14:45 -0400

    Replace comment with docstring. Fixes #423

--- setuptools/__init__.py
+++ setuptools/__init__.py
@@ -123,7 +123,10 @@ class Command(_Command):
     command_consumes_arguments = False
 
     def __init__(self, dist, **kw):
-        # Add support for keyword arguments
+        """
+        Construct the command for dist, updating
+        vars(self) with any keyword parameters.
+        """
         _Command.__init__(self, dist)
         vars(self).update(kw)
 
