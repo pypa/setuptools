421db46870409b6b0cf797dac8869fddc8156191
Jason R. Coombs <jaraco@jaraco.com>
2015-09-06 13:59:27 -0400

    Python 3.6 isn't yet a thing, apparently.

--- setup.py
+++ setup.py
@@ -145,7 +145,6 @@ setup_params = dict(
         Programming Language :: Python :: 3.3
         Programming Language :: Python :: 3.4
         Programming Language :: Python :: 3.5
-        Programming Language :: Python :: 3.6
         Topic :: Software Development :: Libraries :: Python Modules
         Topic :: System :: Archiving :: Packaging
         Topic :: System :: Systems Administration
