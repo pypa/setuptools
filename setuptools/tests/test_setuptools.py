9366bb0996cd7c8d3f77ee45f84ddd77f7ded8b7
Jason R. Coombs <jaraco@jaraco.com>
2015-09-06 22:30:23 -0400

    Add tests capturing expected behavior, including failure to match expectation indicated in docstring.

---
+++ setuptools/tests/test_setuptools.py
@@ -0,0 +1,24 @@
+import pytest
+
+import setuptools
+
+
+@pytest.fixture
+def example_source(tmpdir):
+    tmpdir.mkdir('foo')
+    (tmpdir / 'foo/bar.py').write('')
+    (tmpdir / 'readme.txt').write('')
+    return tmpdir
+
+
+def test_findall(example_source):
+    found = list(setuptools.findall(str(example_source)))
+    expected = ['readme.txt', 'foo/bar.py']
+    assert found == expected
+
+
+def test_findall_curdir(example_source):
+    with example_source.as_cwd():
+        found = list(setuptools.findall())
+    expected = ['readme.txt', 'foo/bar.py']
+    assert found == expected
