998ceceaeb3dfce1b2b05a82aaf356db54f46de6
Jason R. Coombs <jaraco@jaraco.com>
2015-11-24 11:27:22 -0500

    Back out 435b1b762fba. The change breaks detection of zip eggs. Reopens #462.

--- pkg_resources/__init__.py
+++ pkg_resources/__init__.py
@@ -2288,7 +2288,6 @@ def _is_unpacked_egg(path):
     """
     return (
         path.lower().endswith('.egg')
-        and os.path.isdir(os.path.join(path, 'EGG-INFO'))
     )
 
 def _set_parent_ns(packageName):
