477fef2d71db14b82d74ea73f4f90e02876d1967
Jason R. Coombs <jaraco@jaraco.com>
2015-12-14 04:51:03 -0500

    Add test capturing InterpolationSyntaxError on Python 3. Ref #442

--- setuptools/tests/test_packageindex.py
+++ setuptools/tests/test_packageindex.py
@@ -219,6 +219,6 @@ class TestPyPIConfig:
                 password=pity%
             """))
         cfg = setuptools.package_index.PyPIConfig()
-        cred = cfg.creds_by_repository['pypi']
+        cred = cfg.creds_by_repository['https://pypi.python.org']
         assert cred.username == 'jaraco'
         assert cred.password == 'pity%'
