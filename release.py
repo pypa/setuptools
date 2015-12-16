89a239d8a2e2dc0dc42d717ad131957f1063a456
Jason R. Coombs <jaraco@jaraco.com>
2015-12-16 08:04:01 -0500

    Now that bootstrap script automatically installs the latest published version, it's no longer necessary to track the _current_ bootstrap version with a bookmark.

--- release.py
+++ release.py
@@ -4,20 +4,11 @@ install jaraco.packaging and run 'python -m jaraco.packaging.release'
 """
 
 import os
-import subprocess
 
 import pkg_resources
 
 pkg_resources.require('jaraco.packaging>=2.0')
 pkg_resources.require('wheel')
-
-
-def before_upload():
-    BootstrapBookmark.add()
-
-
-def after_push():
-    BootstrapBookmark.push()
 
 files_with_versions = 'setuptools/version.py',
 
@@ -27,22 +18,3 @@ dist_commands = 'sdist', 'bdist_wheel'
 test_info = "Travis-CI tests: http://travis-ci.org/#!/jaraco/setuptools"
 
 os.environ["SETUPTOOLS_INSTALL_WINDOWS_SPECIFIC_FILES"] = "1"
-
-class BootstrapBookmark:
-    name = 'bootstrap'
-
-    @classmethod
-    def add(cls):
-        cmd = ['hg', 'bookmark', '-i', cls.name, '-f']
-        subprocess.Popen(cmd)
-
-    @classmethod
-    def push(cls):
-        """
-        Push the bootstrap bookmark
-        """
-        push_command = ['hg', 'push', '-B', cls.name]
-        # don't use check_call here because mercurial will return a non-zero
-        # code even if it succeeds at pushing the bookmark (because there are
-        # no changesets to be pushed). !dm mercurial
-        subprocess.call(push_command)
