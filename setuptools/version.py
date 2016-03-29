aed09e8a3b4af5472f893a5f702e739a61d94153
Jason R. Coombs <jaraco@jaraco.com>
2016-03-29 14:31:04 -0400

    Add fallback version to enable egg_info bootstrap

--- setuptools/version.py
+++ setuptools/version.py
@@ -1,3 +1,6 @@
 import pkg_resources
 
-__version__ = pkg_resources.require('setuptools')[0].version
+try:
+	__version__ = pkg_resources.require('setuptools')[0].version
+except Exception:
+	__version__ = 'unknown'
