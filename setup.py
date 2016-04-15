f676f1a8e36d4a887a81aba3cba64f2bec850ec7
Jason R. Coombs <jaraco@jaraco.com>
2016-04-15 08:39:13 +0200

    Bump version: 20.7.0 → 20.8.0

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ wheel = ['wheel'] if needs_wheel else []
 
 setup_params = dict(
     name="setuptools",
-    version="20.7.0",
+    version="20.8.0",
     description="Easily download, build, install, upgrade, and uninstall "
                 "Python packages",
     author="Python Packaging Authority",
