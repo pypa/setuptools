232d2d0949022f2a5d175f82afd09739929efa96
Jason R. Coombs <jaraco@jaraco.com>
2016-04-26 12:26:47 -0400

    Re-use implementation as found in setuptools.test.fixtures

--- conftest.py
+++ conftest.py
@@ -2,9 +2,6 @@ import pytest
 
 
 @pytest.yield_fixture
-def tmpdir_as_cwd(tmpdir):
-    try:
-        orig = tmpdir.chdir()
-        yield tmpdir
-    finally:
-        orig.chdir()
+def tmpdir_cwd(tmpdir):
+    with tmpdir.as_cwd() as orig:
+        yield orig
--- test_ez_setup.py
+++ test_ez_setup.py
@@ -1,9 +1,9 @@
 import ez_setup
 
 
-def test_download(tmpdir_as_cwd):
+def test_download(tmpdir_cwd):
     ez_setup.download_setuptools()
-    res, = tmpdir_as_cwd.listdir()
+    res, = tmpdir_cwd.listdir()
     assert res.basename.startswith('setuptools-')
     assert res.basename.endswith('.zip')
     # file should be bigger than 64k
