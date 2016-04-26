c271726ed871c3a34c2d39ff6d67f22570548c21
Jason R. Coombs <jaraco@jaraco.com>
2016-04-26 12:30:51 -0400

    That fixture passes the original dir, so set cwd separately.

--- test_ez_setup.py
+++ test_ez_setup.py
@@ -1,9 +1,12 @@
+import py.path
+
 import ez_setup
 
 
 def test_download(tmpdir_cwd):
+    cwd = py.path.local()
     ez_setup.download_setuptools()
-    res, = tmpdir_cwd.listdir()
+    res, = cwd.listdir()
     assert res.basename.startswith('setuptools-')
     assert res.basename.endswith('.zip')
     # file should be bigger than 64k
