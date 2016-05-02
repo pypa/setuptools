c0f5771495dea58b07f4a1d36badd4cef08aa687
Jason R. Coombs <jaraco@jaraco.com>
2016-05-02 10:20:04 -0400

    Tabs to spaces in two more files. Ref #489.

--- setuptools/version.py
+++ setuptools/version.py
@@ -1,6 +1,6 @@
 import pkg_resources
 
 try:
-	__version__ = pkg_resources.require('setuptools')[0].version
+    __version__ = pkg_resources.require('setuptools')[0].version
 except Exception:
-	__version__ = 'unknown'
+    __version__ = 'unknown'
