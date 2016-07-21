48f4155552b31928ef88b68e25e78db330b8b919
Jason R. Coombs <jaraco@jaraco.com>
2016-07-21 12:15:22 -0400

    Bump version: 24.2.1 → 24.3.0

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ wheel = ['wheel'] if needs_wheel else []
 
 setup_params = dict(
     name="setuptools",
-    version="24.2.1",
+    version="24.3.0",
     description="Easily download, build, install, upgrade, and uninstall "
                 "Python packages",
     author="Python Packaging Authority",
