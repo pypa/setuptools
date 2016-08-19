cea7420c9417f14f6c069dabf1df23055f681af1
Jason R. Coombs <jaraco@jaraco.com>
2016-08-19 16:10:05 -0400

    Bump version: 25.3.0 → 25.4.0

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ def pypi_link(pkg_filename):
 
 setup_params = dict(
     name="setuptools",
-    version="25.3.0",
+    version="25.4.0",
     description="Easily download, build, install, upgrade, and uninstall "
         "Python packages",
     author="Python Packaging Authority",
