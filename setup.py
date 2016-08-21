64727bb4d1eb2a44d2b9a67be8d7c613fe78ccef
Jason R. Coombs <jaraco@jaraco.com>
2016-08-20 20:21:01 -0400

    Issue is fixed, so just exclude the offending version.

--- setup.py
+++ setup.py
@@ -181,7 +181,7 @@ setup_params = dict(
     tests_require=[
         'setuptools[ssl]',
         'pytest-flake8',
-        'pytest>=2.8,<3.0dev',
+        'pytest>=2.8,!=3.0.0',
     ] + (['mock'] if sys.version_info[:2] < (3, 3) else []),
     setup_requires=[
     ] + pytest_runner + wheel,
