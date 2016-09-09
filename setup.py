1aa1d6c94b108147030fdf11057df3251b7eefda
Jason R. Coombs <jaraco@jaraco.com>
2016-09-09 10:51:34 -0400

    Bump version: 27.0.0 → 27.1.0

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ def pypi_link(pkg_filename):
 
 setup_params = dict(
     name="setuptools",
-    version="27.0.0",
+    version="27.1.0",
     description="Easily download, build, install, upgrade, and uninstall "
         "Python packages",
     author="Python Packaging Authority",
