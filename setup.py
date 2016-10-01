cf469bf239cc6c0d4ebb766c5e2b6c6098e2b28b
Jason R. Coombs <jaraco@jaraco.com>
2016-10-01 17:40:19 -0400

    Pin to Pytest 3.0.2 as it appears 3.0.3 re-introduces pytest-dev/pytest#1888.

--- setup.py
+++ setup.py
@@ -181,7 +181,8 @@ setup_params = dict(
     tests_require=[
         'setuptools[ssl]',
         'pytest-flake8',
-        'pytest>=3.0.2',
+        # pin pytest to 3.0.2 for pytest-dev/pytest#1888
+        'pytest==3.0.2',
     ] + (['mock'] if sys.version_info[:2] < (3, 3) else []),
     setup_requires=[
     ] + pytest_runner + wheel,
