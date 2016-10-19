d7c802d072989f6543f584fa6dfdd57b140ca43e
Jason R. Coombs <jaraco@jaraco.com>
2016-10-19 11:32:51 -0400

    Bump version: 28.6.0 → 28.6.1

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ def pypi_link(pkg_filename):
 
 setup_params = dict(
     name="setuptools",
-    version="28.6.0",
+    version="28.6.1",
     description="Easily download, build, install, upgrade, and uninstall "
         "Python packages",
     author="Python Packaging Authority",
