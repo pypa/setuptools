b890d3d27602b6c18985c735a0ba0933232deb82
Jason R. Coombs <jaraco@jaraco.com>
2016-12-01 09:54:40 -0500

    Clean up conftest.py

--- conftest.py
+++ conftest.py
@@ -1,8 +1,7 @@
-import pytest
-
 pytest_plugins = 'setuptools.tests.fixtures'
 
 def pytest_addoption(parser):
-      parser.addoption("--package_name", action="append", default=[],
-                       help="list of package_name to pass to test functions")
-
+    parser.addoption(
+        "--package_name", action="append", default=[],
+        help="list of package_name to pass to test functions",
+    )
