17f752d3be0b2f89b405976364653f081f522fb3
Jason R. Coombs <jaraco@jaraco.com>
2016-12-08 09:54:03 -0500

    Bump version: 30.2.0 → 30.2.1

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ def pypi_link(pkg_filename):
 
 setup_params = dict(
     name="setuptools",
-    version="30.2.0",
+    version="30.2.1",
     description="Easily download, build, install, upgrade, and uninstall "
         "Python packages",
     author="Python Packaging Authority",
