a347ec5a6c25800be066db358d96f2f17ccbaae5
Jason R. Coombs <jaraco@jaraco.com>
2017-01-01 10:51:50 -0500

    In bootstrap script, remove support for re-using a download in the cwd.

--- test_ez_setup.py
+++ test_ez_setup.py
@@ -1,8 +1,4 @@
-from zipfile import BadZipfile
-
 import py.path
-
-import pytest
 
 import ez_setup
 
@@ -15,15 +11,3 @@ def test_download(tmpdir_cwd):
     assert res.basename.endswith('.zip')
     # file should be bigger than 64k
     assert res.size() > 2**16
-
-
-def test_message_corrupted_setuptools(tmpdir_cwd):
-    cwd = py.path.local()
-    ez_setup.download_setuptools()
-    res, = cwd.listdir()
-    res.write('CORRUPT ME')
-    with pytest.raises(BadZipfile) as excinfo:
-        with ez_setup.archive_context(res.strpath):
-            pass
-    msg = ez_setup.MEANINGFUL_INVALID_ZIP_ERR_MSG.format(res.strpath)
-    assert msg in str(excinfo.value)
