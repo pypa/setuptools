79919e21fd1947749959c977d2bb05645e7829e0
Hugo <hugovk@users.noreply.github.com>
2017-01-17 22:28:20 +0200

    Update Python versions

--- setup.py
+++ setup.py
@@ -145,12 +145,14 @@ setup_params = dict(
         Intended Audience :: Developers
         License :: OSI Approved :: MIT License
         Operating System :: OS Independent
+        Programming Language :: Python :: 2
         Programming Language :: Python :: 2.6
         Programming Language :: Python :: 2.7
         Programming Language :: Python :: 3
         Programming Language :: Python :: 3.3
         Programming Language :: Python :: 3.4
         Programming Language :: Python :: 3.5
+        Programming Language :: Python :: 3.6
         Topic :: Software Development :: Libraries :: Python Modules
         Topic :: System :: Archiving :: Packaging
         Topic :: System :: Systems Administration
