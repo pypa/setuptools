f8cb9c6547de40234c67ba1caf56a7b63d11f07b
Alex Chan <a.chan@wellcome.ac.uk>
2017-06-01 14:48:46 +0100

    Add a test that setuptools can be installed in a clean environment

---
+++ clean_install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o xtrace
+
+# Create a temporary directory to install the virtualenv in
+VENV_DIR="$(mktemp -d)"
+function cleanup { rm -rf "$VENV_DIR" }
+trap cleanup EXIT
+
+# Create a virtualenv that doesn't have pip or setuptools installed
+wget https://raw.githubusercontent.com/pypa/virtualenv/master/virtualenv.py
+python virtualenv.py --no-wheel --no-pip --no-setuptools "$VENV_DIR"
+source "$VENV_DIR/bin/activate"
+
+# Now try to install setuptools
+python bootstrap.py
+python setup.py install
