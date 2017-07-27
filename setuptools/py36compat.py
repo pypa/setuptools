1535d29c523a5207614adf7e1af9db1411a95a79
Jason R. Coombs <jaraco@jaraco.com>
2017-07-27 09:58:30 -0400

    Also disable interpolation when on Python 2. Ref #889

--- setuptools/py36compat.py
+++ setuptools/py36compat.py
@@ -3,11 +3,14 @@ from distutils.errors import DistutilsOptionError
 from distutils.util import strtobool
 from distutils.debug import DEBUG
 
+import six
+
 
 class Distribution_parse_config_files:
     """
-    Mix-in providing forward-compatibility for functionality to be
-    included by default on Python 3.7.
+    Mix-in providing forward-compatibility for functionality
+    presumed to be included by default on Python 3.7. See
+    http://bugs.python.org/issue20754 and #889 for details.
 
     Do not edit the code in this class except to update functionality
     as implemented in distutils.
@@ -68,15 +71,62 @@ class Distribution_parse_config_files:
                 except ValueError as msg:
                     raise DistutilsOptionError(msg)
 
+    def parse_config_files_py2(self, filenames=None):
+        """
+        Backport of the Python 3 implementation. Since `interpolation`
+        isn't available as a parameter, use the RawConfigParser instead.
+        """
+        from ConfigParser import RawConfigParser
 
-if sys.version_info < (3,):
-    # Python 2 behavior is sufficient
-    class Distribution_parse_config_files:
-        pass
+        # no venv on Python 2
+        ignore_options = frozenset([])
+
+        if filenames is None:
+            filenames = self.find_config_files()
+
+        if DEBUG:
+            self.announce("Distribution.parse_config_files():")
+
+        parser = RawConfigParser()
+        for filename in filenames:
+            if DEBUG:
+                self.announce("  reading %s" % filename)
+            parser.read(filename)
+            for section in parser.sections():
+                options = parser.options(section)
+                opt_dict = self.get_option_dict(section)
+
+                for opt in options:
+                    if opt != '__name__' and opt not in ignore_options:
+                        val = parser.get(section,opt)
+                        opt = opt.replace('-', '_')
+                        opt_dict[opt] = (filename, val)
+
+            # Make the ConfigParser forget everything (so we retain
+            # the original filenames that options come from)
+            parser.__init__()
+
+        # If there was a "global" section in the config file, use it
+        # to set Distribution options.
+
+        if 'global' in self.command_options:
+            for (opt, (src, val)) in self.command_options['global'].items():
+                alias = self.negative_opt.get(opt)
+                try:
+                    if alias:
+                        setattr(self, alias, not strtobool(val))
+                    elif opt in ('verbose', 'dry_run'): # ugh!
+                        setattr(self, opt, strtobool(val))
+                    else:
+                        setattr(self, opt, val)
+                except ValueError as msg:
+                    raise DistutilsOptionError(msg)
+
+    if six.PY2:
+        parse_config_files = parse_config_files_py2
 
 
 if False:
     # When updated behavior is available upstream,
     # disable override here.
-    class Distribution_parse_config_files:
-        pass
+    del Distribution_parse_config_files.parse_config_files
