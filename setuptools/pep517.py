5db94ea1343565c64e15069379f671d0daeba96c
xoviat <xoviat@users.noreply.github.com>
2017-09-03 20:28:28 -0500

    pep517: add prepare_metadata_for_build_wheel

--- setuptools/pep517.py
+++ setuptools/pep517.py
@@ -11,44 +11,14 @@ from setuptools.dist import SetupRequirementsError
 
 SETUPTOOLS_IMPLEMENTATION_REVISION = 0.1
 
-
-class TemporaryBuildDirectory(object):
-    def __init__(self):
-        self._tmpdir = tempfile.mkdtemp(
-            prefix='setuptools')
-        self._cwd = os.getcwd()
-        
-    def __enter__(self):
-        """Enter the temporary directory 
-        
-        Please remember to obtain abs paths 
-        before entering
-        """
-        # Get list of directories to copy
-        directories_to_copy = [
-            d for d in os.listdir('.') if not d.startswith('.')]
-        for directory in directories_to_copy:
-            shutil.copytree(
-                os.path.join(self._cwd, directory),
-                self._tmpdir)
-            
-        # Now enter the directory
-        os.chdir(self._tmpdir.name)
-        
-    def __exit__(self):
-        os.chdir(self._cwd)
-        shutil.rmtree(self._tmpdir)
-
-
 def _run_setup(setup_script='setup.py'): #
     # Note that we can reuse our build directory between calls
     # Correctness comes first, then optimization later    
-    with TemporaryBuildDirectory():
-        __file__=setup_script
-        f=getattr(tokenize, 'open', open)(__file__)
-        code=f.read().replace('\\r\\n', '\\n')
-        f.close()
-        exec(compile(code, __file__, 'exec'))
+    __file__=setup_script
+    f=getattr(tokenize, 'open', open)(__file__)
+    code=f.read().replace('\\r\\n', '\\n')
+    f.close()
+    exec(compile(code, __file__, 'exec'))
 
 
 def get_build_requires(config_settings):
@@ -75,9 +45,13 @@ def get_requires_for_build_sdist(config_settings=None):
     return get_build_requires(config_settings)
 
 
+def prepare_metadata_for_build_wheel(metadata_directory, config_settings=None):
+    sys.argv = sys.argv[:1] + ['dist_info', '--egg-base', metadata_directory]
+    _run_setup()
+
 def build_wheel(wheel_directory, config_settings=None,
                 metadata_directory=None):
-    wheel_directory = os.abspath(wheel_directory)
+    wheel_directory = os.path.abspath(wheel_directory)
     sys.argv = sys.argv[:1] + ['bdist_wheel'] + \
         config_settings["--global-option"]
     _run_setup()
@@ -87,7 +61,7 @@ def build_wheel(wheel_directory, config_settings=None,
 
 
 def build_sdist(sdist_directory, config_settings=None):
-    sdist_directory = os.abspath(sdist_directory)
+    sdist_directory = os.path.abspath(sdist_directory)
     sys.argv = sys.argv[:1] + ['sdist'] + \
         config_settings["--global-option"]
     _run_setup()
