9f295a706590b1e3618978d6f2d83af0b893ec4d
Jason R. Coombs <jaraco@jaraco.com>
2017-09-15 10:19:29 -0400

    Bump version: 36.4.0 → 36.5.0

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ def pypi_link(pkg_filename):
 
 setup_params = dict(
     name="setuptools",
-    version="36.4.0",
+    version="36.5.0",
     description="Easily download, build, install, upgrade, and uninstall "
         "Python packages",
     author="Python Packaging Authority",
