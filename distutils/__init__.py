7a752370d69c7df139425a205984a77bb088824f
xoviat <xoviat@users.noreply.github.com>
2017-11-20 16:53:13 -0600

    [debug] remove exception

--- distutils/__init__.py
+++ distutils/__init__.py
@@ -14,7 +14,5 @@ import os
 if sys.version_info < (3, 6):
     __path__ = __import__('pkgutil').extend_path(__path__, __name__)
     __path__ = [p for p in __path__ if os.path.normpath(p) != os.path.dirname(__file__)]
-
-    raise Exception(__path__)
 else:
     __version__ = sys.version[:sys.version.index(' ')]
