20a9dfe6d3cf0f1d1c7168bf0935b418d16d46e3
xoviat <xoviat@users.noreply.github.com>
2017-11-20 16:47:50 -0600

    [debug] filter paths

--- distutils/__init__.py
+++ distutils/__init__.py
@@ -11,7 +11,8 @@ used from a setup script as
 import sys
 
 if sys.version_info < (3, 6):
-    __path__ = __import__('pkgutil').extend_path([], __name__)
+    __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+    __path__ = [p for p in __path__ if os.path.normpath(p) != os.path.dirname(__file__)]
 
     raise Exception(__path__)
 else:
