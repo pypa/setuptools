1709fd1ebdc020c48f8670976894af4221601b45
xoviat <xoviat@users.noreply.github.com>
2017-12-01 16:45:32 -0600

    dist_info: make dist_info a property of self

--- setuptools/command/dist_info.py
+++ setuptools/command/dist_info.py
@@ -20,6 +20,7 @@ class dist_info(Command):
 
     def initialize_options(self):
         self.egg_base = None
+        self.dist_info = None
 
     def finalize_options(self):
         pass
@@ -29,8 +30,8 @@ class dist_info(Command):
         egg_info.egg_base = self.egg_base
         egg_info.finalize_options()
         egg_info.run()
-        dist_info_dir = egg_info.egg_info[:-len('.egg-info')] + '.dist-info'
-        log.info("creating '{}'".format(os.path.abspath(dist_info_dir)))
+        self.dist_info = egg_info.egg_info[:-len('.egg-info')] + '.dist-info'
+        log.info("creating '{}'".format(os.path.abspath(self.dist_info)))
 
         bdist_wheel = self.get_finalized_command('bdist_wheel')
-        bdist_wheel.egg2dist(egg_info.egg_info, dist_info_dir)
+        bdist_wheel.egg2dist(egg_info.egg_info, self.dist_info)
