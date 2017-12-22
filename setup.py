0d0baed8da468b2cedb9aa6caf60dc15fdeebd34
Doug Greiman <dgreiman@google.com>
2017-12-22 15:04:22 -0800

    Bump version: 38.2.4 → 38.2.5

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ def pypi_link(pkg_filename):
 
 setup_params = dict(
     name="setuptools",
-    version="38.2.4",
+    version="38.2.5",
     description="Easily download, build, install, upgrade, and uninstall "
         "Python packages",
     author="Python Packaging Authority",
