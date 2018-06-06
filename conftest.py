445d38472d3dc2fd004b02d645e7eadcf26fbc30
Jason R. Coombs <jaraco@jaraco.com>
2018-06-06 08:38:07 -0400

    Move ignores into conftest.py

--- conftest.py
+++ conftest.py
@@ -6,3 +6,15 @@ def pytest_addoption(parser):
         "--package_name", action="append", default=[],
         help="list of package_name to pass to test functions",
     )
+
+
+collect_ignore = [
+    'release.py',
+    'setuptools/lib2to3_ex.py',
+    'tests/manual_test.py',
+    'tests/test_pypi.py',
+    'tests/shlib_test',
+    'scripts/upload-old-releases-as-zip.py',
+    'pavement.py',
+    'setuptools/tests/mod_with_constant.py',
+]
