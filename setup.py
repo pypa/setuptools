dc9b2b6bdb2e53582ed7d090f40a1aec5983fd3a
Nikolaus Waxweiler <madigens@gmail.com>
2018-10-27 10:27:56 +0100

    Add Python 3.7 to the classifiers
    Fixes https://github.com/pypa/setuptools/issues/1528.

--- setup.py
+++ setup.py
@@ -164,6 +164,7 @@ setup_params = dict(
         Programming Language :: Python :: 3.4
         Programming Language :: Python :: 3.5
         Programming Language :: Python :: 3.6
+        Programming Language :: Python :: 3.7
         Topic :: Software Development :: Libraries :: Python Modules
         Topic :: System :: Archiving :: Packaging
         Topic :: System :: Systems Administration
