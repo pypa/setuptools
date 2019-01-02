2bb8225a6efa6f429d4522f47308b779a48cd07f
Jason R. Coombs <jaraco@jaraco.com>
2019-01-02 14:42:23 -0500

    Extract common behavior into a function

--- setuptools/build_meta.py
+++ setuptools/build_meta.py
@@ -149,6 +149,15 @@ def prepare_metadata_for_build_wheel(metadata_directory, config_settings=None):
     return dist_infos[0]
 
 
+def _file_with_extension(directory, extension):
+    matching = (
+        f for f in os.listdir(directory)
+        if f.endswith(extension)
+    )
+    file, = matching
+    return file
+
+
 def build_wheel(wheel_directory, config_settings=None,
                 metadata_directory=None):
     config_settings = _fix_config(config_settings)
@@ -160,13 +169,7 @@ def build_wheel(wheel_directory, config_settings=None,
         shutil.rmtree(wheel_directory)
         shutil.copytree('dist', wheel_directory)
 
-    wheels = (
-        f for f in os.listdir(wheel_directory)
-        if f.endswith('.whl')
-    )
-
-    wheel, = wheels
-    return wheel
+    return _file_with_extension(wheel_directory, '.whl')
 
 
 def build_sdist(sdist_directory, config_settings=None):
@@ -177,10 +180,4 @@ def build_sdist(sdist_directory, config_settings=None):
         ["--dist-dir", sdist_directory]
     _run_setup()
 
-    sdists = (
-        f for f in os.listdir(sdist_directory)
-        if f.endswith('.tar.gz')
-    )
-
-    sdist, = sdists
-    return sdist
+    return _file_with_extension(sdist_directory, '.tar.gz')
