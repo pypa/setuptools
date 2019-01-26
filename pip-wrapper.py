ff950773759423085f506ced0cc8f0f77afff5d1
Jason R. Coombs <jaraco@jaraco.com>
2019-01-26 17:05:25 -0500

    Fix pip upgrade/downgrade on Windows.

--- pip-wrapper.py
+++ pip-wrapper.py
@@ -12,7 +12,13 @@ import subprocess
 
 
 def ensure_pip_version(pip_bin, ver):
-    cmd = [pip_bin, 'install', 'pip ' + ver]
+    """
+    Ensure the pip version meets the specified version.
+
+    Use python -m pip to upgrade/downgrade, because for this operation,
+    on Windows, pip.exe must not be locked.
+    """
+    cmd = [sys.executable, '-m', 'pip', 'install', 'pip ' + ver]
     subprocess.check_call(cmd)
 
 
