1c187ad0cf50fbc14626f63cb669a9ec5949012f
Jason R. Coombs <jaraco@jaraco.com>
2019-09-09 17:45:58 +0100

    List sidebars to avoid errors looking for template 't'

--- docs/conf.py
+++ docs/conf.py
@@ -2,4 +2,4 @@
 # Custom sidebar templates, maps document names to template names.
 html_theme = 'alabaster'
 templates_path = ['_templates']
-html_sidebars = {'index': 'tidelift-sidebar.html'}
+html_sidebars = {'index': ['tidelift-sidebar.html']}
