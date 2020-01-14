18a3cae3513818d355dbc8c05ff93bbcee09a6d5
Jason R. Coombs <jaraco@jaraco.com>
2020-01-14 04:14:13 -0500

    Update Python 2 warning to include a minimum sunset date and add a preamble to make referencing the warning more reliable. Ref #1458.

--- pkg_resources/py2_warn.py
+++ pkg_resources/py2_warn.py
@@ -7,7 +7,8 @@ msg = textwrap.dedent("""
     You are running Setuptools on Python 2, which is no longer
     supported and
     >>> SETUPTOOLS WILL STOP WORKING <<<
-    in a subsequent release. Please ensure you are installing
+    in a subsequent release (no sooner than 2020-04-20).
+    Please ensure you are installing
     Setuptools using pip 9.x or later or pin to `setuptools<45`
     in your environment.
     If you have done those things and are still encountering
@@ -16,4 +17,6 @@ msg = textwrap.dedent("""
     about the steps that led to this unsupported combination.
     """)
 
-sys.version_info < (3,) and warnings.warn("*" * 60 + msg + "*" * 60)
+pre = "Setuptools will stop working on Python 2\n"
+
+sys.version_info < (3,) and warnings.warn(pre + "*" * 60 + msg + "*" * 60)
