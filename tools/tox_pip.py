6d4e23882a5b1e1f31fb452aaad9d19cf0d02604
Jason R. Coombs <jaraco@jaraco.com>
2020-01-19 14:06:35 -0500

    Move test dependencies to package metadata.

--- tools/tox_pip.py
+++ tools/tox_pip.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+import re
 
 
 def remove_setuptools():
@@ -20,12 +21,29 @@ def bootstrap():
     subprocess.check_call(cmd)
 
 
+def is_install_self(args):
+    """
+    Do the args represent an install of .?
+    """
+    def strip_extras(arg):
+        match = re.match(r'(.*)?\[.*\]$', arg)
+        return match.group(1) if match else arg
+
+    return (
+        'install' in args
+        and any(
+            arg in ['.', os.getcwd()]
+            for arg in map(strip_extras, args)
+        )
+    )
+
+
 def pip(args):
     # Honor requires-python when installing test suite dependencies
     if any('-r' in arg for arg in args):
         os.environ['PIP_IGNORE_REQUIRES_PYTHON'] = '0'
 
-    if '.' in args:
+    if is_install_self(args):
         remove_setuptools()
         bootstrap()
 
