b1abc23f0124de8ad99149674613ff8e2c71706a
Jason R. Coombs <jaraco@jaraco.com>
2020-03-08 16:37:43 -0400

    Replace playbook with code for finalizing a release.

---
+++ tools/finalize.py
@@ -0,0 +1,59 @@
+"""
+Finalize the repo for a release. Invokes towncrier and bumpversion.
+"""
+
+__requires__ = ['bump2version', 'towncrier']
+
+
+import subprocess
+import pathlib
+import re
+import sys
+
+
+def release_kind():
+    """
+    Determine which release to make based on the files in the
+    changelog.
+    """
+    # use min here as 'major' < 'minor' < 'patch'
+    return min(
+        'major' if 'breaking' in file.name else
+        'minor' if 'change' in file.name else
+        'patch'
+        for file in pathlib.Path('changelog.d').iterdir()
+    )
+
+
+bump_version_command = [
+    sys.executable,
+    '-m', 'bumpversion',
+    release_kind(),
+]
+
+
+def get_version():
+    cmd = bump_version_command + ['--dry-run', '--verbose']
+    out = subprocess.check_output(cmd, text=True)
+    return re.search('^new_version=(.*)', out, re.MULTILINE).group(1)
+
+
+def update_changelog():
+    cmd = [
+        sys.executable, '-m',
+        'towncrier',
+        '--version', get_version(),
+        '--yes',
+    ]
+    subprocess.check_call(cmd)
+
+
+def bump_version():
+    cmd = bump_version_command + ['--allow-dirty']
+    subprocess.check_call(cmd)
+
+
+if __name__ == '__main__':
+    print("Cutting release at", get_version())
+    update_changelog()
+    bump_version()
