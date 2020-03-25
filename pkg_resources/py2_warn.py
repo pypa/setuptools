734eec6e3fca725c6b18cad2fffbd1f3e3a56fa7
Jason R. Coombs <jaraco@jaraco.com>
2020-03-25 10:57:15 -0400

    Update warning message to direct users to file a template instead of commenting in #1458.

--- pkg_resources/py2_warn.py
+++ pkg_resources/py2_warn.py
@@ -12,9 +12,8 @@ msg = textwrap.dedent("""
     Setuptools using pip 9.x or later or pin to `setuptools<45`
     in your environment.
     If you have done those things and are still encountering
-    this message, please comment in
-    https://github.com/pypa/setuptools/issues/1458
-    about the steps that led to this unsupported combination.
+    this message, please follow up at
+    https://bit.ly/setuptools-py2-warning.
     """)
 
 pre = "Setuptools will stop working on Python 2\n"
