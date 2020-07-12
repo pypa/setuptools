9e58d95017656ee33d2cf642cf1be6c77298f16d
Jason R. Coombs <jaraco@jaraco.com>
2020-07-12 10:56:14 -0400

    Re-enable distutils patch by default.

--- setuptools/distutils_patch.py
+++ setuptools/distutils_patch.py
@@ -25,7 +25,7 @@ def enabled():
     """
     Allow selection of distutils by environment variable.
     """
-    which = os.environ.get('SETUPTOOLS_USE_DISTUTILS', 'stdlib')
+    which = os.environ.get('SETUPTOOLS_USE_DISTUTILS', 'local')
     return which == 'local'
 
 
