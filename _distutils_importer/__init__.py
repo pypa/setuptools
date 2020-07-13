89e9d3c8910c3f419eb9f1c2758a748c6938655b
Paul Ganssle <paul@ganssle.io>
2020-07-13 14:43:23 -0400

    Adjust distutils shim when removing _distutils_importer

--- _distutils_importer/__init__.py
+++ _distutils_importer/__init__.py
@@ -1,6 +1,17 @@
 import sys
 
-here = os.path.dirname(__file__)
-NEW_DISTUTILS_LOCATION = os.path.join(here, 'distutils-shim-package')
+_HERE = os.path.dirname(__file__)
+NEW_DISTUTILS_LOCATION = os.path.join(_HERE, 'distutils-shim-package')
 
-sys.path.insert(0, NEW_DISTUTILS_LOCATION)
+def add_shim():
+    if NEW_DISTUTILS_LOCATION not in sys.path:
+        sys.path.insert(0, NEW_DISTUTILS_LOCATION)
+
+def remove_shim():
+    try:
+        sys.path.remove(NEW_DISTUTILS_LOCATION)
+    except ValueError:
+        pass
+
+
+add_shim()
