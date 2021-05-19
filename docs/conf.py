d3cc031edaa843ad796c5e1e079c1e4b58bcec06
Jason R. Coombs <jaraco@jaraco.com>
2021-05-19 11:44:40 -0400

    Avoid publishing unreleased changes when there are none.

--- docs/conf.py
+++ docs/conf.py
@@ -148,3 +148,5 @@ intersphinx_mapping.update(
 extensions += ['sphinxcontrib.towncrier']
 # Extension needs a path from here to the towncrier config.
 towncrier_draft_working_directory = '..'
+# Avoid an empty section for unpublished changes.
+towncrier_draft_include_empty = False
