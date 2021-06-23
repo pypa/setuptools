026bb95d9d1498a94e35d20443bc1fb3a4cbdfd5
Sorin Sbarnea <ssbarnea@redhat.com>
2021-06-23 12:11:15 +0100

    Avoid infinite loops with symlinks
    Fixes: #332

--- setuptools/_distutils/filelist.py
+++ setuptools/_distutils/filelist.py
@@ -247,12 +247,22 @@ def _find_all_simple(path):
     """
     Find all files under 'path'
     """
-    results = (
-        os.path.join(base, file)
-        for base, dirs, files in os.walk(path, followlinks=True)
-        for file in files
-    )
-    return filter(os.path.isfile, results)
+    # https://bugs.python.org/issue44497
+    dirs = set()
+    files = set()
+    for dirpath, dirnames, filenames in os.walk(path, followlinks=True):
+        st = os.stat(dirpath)
+        scandirs = []
+        for dirname in dirnames:
+            st = os.stat(os.path.join(dirpath, dirname))
+            dirkey = st.st_dev, st.st_ino
+            if dirkey not in dirs:
+                dirs.add(dirkey)
+                scandirs.append(dirname)
+        dirnames[:] = scandirs
+        for f in filenames:
+            files.add(os.path.join(dirpath, f))
+    return files
 
 
 def findall(dir=os.curdir):
