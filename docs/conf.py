918a415b41225f442d6e9b319ecebff19b52a1a5
Jason R. Coombs <jaraco@jaraco.com>
2021-07-18 14:48:16 -0400

    Implement tidelift banner using a Sphinx directive implemented by jaraco.tidelift.

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,1 @@
-
-# Custom sidebar templates, maps document names to template names.
-html_theme = 'alabaster'
-templates_path = ['_templates']
-html_sidebars = {'index': ['tidelift-sidebar.html']}
+extensions += ['jaraco.tidelift']
