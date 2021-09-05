1bcfd8107af3b42ae1f423770c1a1381901a4e77
Jason R. Coombs <jaraco@jaraco.com>
2021-09-05 14:25:48 -0400

    Expand the vendored dependencies.

--- _setuptools_vendored/__init__.py
+++ _setuptools_vendored/__init__.py
@@ -4,4 +4,4 @@ Ensure Setuptools dependencies are present.
 import sys
 import pathlib
 
-sys.path.extend(map(str, pathlib.Path(__file__).parent.glob('*.whl')))
+sys.path.append(str(pathlib.Path(__file__).parent))
