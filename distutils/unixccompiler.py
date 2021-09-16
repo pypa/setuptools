f182b5e2f6e492cd13eed98d7f248d4146456d8c
Isuru Fernando <isuruf@gmail.com>
2021-09-16 14:57:08 -0500

    Split CC to make sure we get the correct basename of the compiler

--- distutils/unixccompiler.py
+++ distutils/unixccompiler.py
@@ -231,7 +231,7 @@ class UnixCCompiler(CCompiler):
         # this time, there's no way to determine this information from
         # the configuration data stored in the Python installation, so
         # we use this hack.
-        compiler = os.path.basename(sysconfig.get_config_var("CC"))
+        compiler = os.path.basename(sysconfig.get_config_var("CC").split(" ")[0])
         if sys.platform[:6] == "darwin":
             from distutils.util import get_macosx_target_ver, split_version
             macosx_target_ver = get_macosx_target_ver()
