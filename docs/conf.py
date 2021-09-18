8ea55f2fb26bd77997f0e9435bab2d41376a76d4
Jason R. Coombs <jaraco@jaraco.com>
2021-09-17 21:23:38 -0400

    Add intersphinx mappings for Python to prevent spurious nitpicky failures. Fixes jaraco/skeleton#51.

--- docs/conf.py
+++ docs/conf.py
@@ -27,3 +27,10 @@ link_files = {
 
 # Be strict about any broken references:
 nitpicky = True
+
+# Include Python intersphinx mapping to prevent failures
+# jaraco/skeleton#51
+extensions += ['sphinx.ext.intersphinx']
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+}
