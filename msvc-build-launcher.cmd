c09d73e04b4a7bdece2fc2692cbd7b675852bffa
Niyas Sait <niyas.sait@linaro.org>
2021-09-30 06:54:03 +0100

    remove arm launcher compilation from msvc-build-launcher.cmd

--- msvc-build-launcher.cmd
+++ msvc-build-launcher.cmd
@@ -34,16 +34,3 @@ if "%ERRORLEVEL%"=="0" (
 ) else (
   echo Windows SDK 6.1 not found to build Windows 64-bit version
 )
-
-REM now for arm 64-bit
-REM Cross compile for arm64
-call VCVARSx86_arm64
-if "%ERRORLEVEL%"=="0" (
-  cl /D "GUI=0" /D "WIN32_LEAN_AND_MEAN" launcher.c /O2 /link /MACHINE:arm64 /SUBSYSTEM:CONSOLE /out:setuptools/cli-arm64.exe
-  cl /D "GUI=1" /D "WIN32_LEAN_AND_MEAN" launcher.c /O2 /link /MACHINE:arm64 /SUBSYSTEM:WINDOWS /out:setuptools/gui-arm64.exe
-) else (
-  echo Windows SDK 6.1 not found to build Windows 64-bit version
-)
-
-set PATH=%PATH_OLD%
-
