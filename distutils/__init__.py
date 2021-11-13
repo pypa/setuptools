6d7976cd57e1f352287869e9ff46e4b3aa3fe333
Jason R. Coombs <jaraco@jaraco.com>
2021-11-13 10:13:25 -0500

    Allow overrides of _multiarch_addendum and _sysconfig_name_tmpl, supporting the patches used by pkgsrc. Fixes #16.

--- distutils/__init__.py
+++ distutils/__init__.py
@@ -15,9 +15,10 @@ __version__ = sys.version[:sys.version.index(' ')]
 
 
 try:
-    # Allow Debian (only) to customize system behavior.
-    # Ref pypa/distutils#2. This hook is deprecated and
-    # no other environments should use it.
+    # Allow Debian and pkgsrc (only) to customize system
+    # behavior. Ref pypa/distutils#2 and pypa/distutils#16.
+    # This hook is deprecated and no other environments
+    # should use it.
     importlib.import_module('_distutils_system_mod')
 except ImportError:
     pass
