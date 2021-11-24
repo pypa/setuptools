f76901831084c11ec633eb96c310860d15199edd
Jason R. Coombs <jaraco@jaraco.com>
2021-11-24 11:57:52 -0500

    Revert "Update comment for _distutils_system_mod." as Fedora is not using that hook.
    This reverts commit 8c64fdc8560d9f7b7d3926350bba7702b0906329.

--- distutils/__init__.py
+++ distutils/__init__.py
@@ -15,10 +15,8 @@ __version__ = sys.version[:sys.version.index(' ')]
 
 
 try:
-    # Allow Debian and pkgsrc and Fedora (only) to customize
-    # system
-    # behavior. Ref pypa/distutils#2 and pypa/distutils#16
-    # and pypa/distutils#70.
+    # Allow Debian and pkgsrc (only) to customize system
+    # behavior. Ref pypa/distutils#2 and pypa/distutils#16.
     # This hook is deprecated and no other environments
     # should use it.
     importlib.import_module('_distutils_system_mod')
