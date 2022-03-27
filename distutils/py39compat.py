8f8d6555aea42186c866d8350738de57ff47eb21
Jason R. Coombs <jaraco@jaraco.com>
2022-03-27 14:50:54 -0400

    Just modify the vars in place.

--- distutils/py39compat.py
+++ distutils/py39compat.py
@@ -7,8 +7,8 @@ def ext_suffix(vars):
     Ensure vars contains 'EXT_SUFFIX'. pypa/distutils#130
     """
     if sys.version_info < (3, 10):
-        return vars
+        return
     if platform.system() != 'Windows':
-        return vars
+        return
     import _imp
     vars.update(EXT_SUFFIX=_imp.extension_suffixes()[0])
