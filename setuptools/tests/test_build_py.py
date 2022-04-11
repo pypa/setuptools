34ae9d8f0f9d8df8f1d530129db92c8164383bb3
Anderson Bravalheri <andersonbravalheri@gmail.com>
2022-04-11 19:23:00 +0100

    Test options.packages.find.exclude with include_package_data for build_py

--- setuptools/tests/test_build_py.py
+++ setuptools/tests/test_build_py.py
@@ -3,8 +3,12 @@ import stat
 import shutil
 
 import pytest
+import jaraco.path
+from path import Path
 
 from setuptools.dist import Distribution
+
+from .textwrap import DALS
 
 
 def test_directories_in_package_data_glob(tmpdir_cwd):
@@ -79,3 +83,50 @@ def test_executable_data(tmpdir_cwd):
 
     assert os.stat('build/lib/pkg/run-me').st_mode & stat.S_IEXEC, \
         "Script is not executable"
+
+
+def test_excluded_subpacakges(tmp_path):
+    files = {
+        "setup.cfg": DALS("""
+            [metadata]
+            name = mypkg
+            version = 42
+
+            [options]
+            include_package_data = True
+            packages = find:
+
+            [options.packages.find]
+            exclude = *.tests*
+            """),
+        "mypkg": {
+            "__init__.py": "",
+            "resource_file.txt": "",
+            "tests": {
+                "__init__.py": "",
+                "test_mypkg.py": "",
+                "test_file.txt": "",
+            }
+        },
+        "MANIFEST.in": DALS("""
+            global-include *.py *.txt
+            global-exclude *.py[cod]
+            prune dist
+            prune build
+            prune *.egg-info
+            """)
+    }
+
+    with Path(tmp_path):
+        jaraco.path.build(files)
+        dist = Distribution({"script_name": "%PEP 517%"})
+        dist.parse_config_files()
+        dist.run_command("build_py")
+        build_dir = Path(dist.get_command_obj("build_py").build_lib)
+
+        assert (build_dir / "mypkg/__init__.py").exists()
+        assert (build_dir / "mypkg/resource_file.txt").exists()
+        assert not (build_dir / "mypkg/tests/__init__.py").exists()
+        assert not (build_dir / "mypkg/tests/test_mypkg.py").exists()
+        assert not (build_dir / "mypkg/tests/test_file.txt").exists()
+        assert not (build_dir / "mypkg/tests").exists()
