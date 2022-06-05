a4f5b769793af19f7b858816889c1bf026f55f5c
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-06-05 04:47:15 +0300

    Update base URL for PEPs (#61)

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ link_files = {
             ),
             dict(
                 pattern=r'PEP[- ](?P<pep_number>\d+)',
-                url='https://www.python.org/dev/peps/pep-{pep_number:0>4}/',
+                url='https://peps.python.org/pep-{pep_number:0>4}/',
             ),
         ],
     )
