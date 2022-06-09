7a68599ca8794d1d1b7d358e6f79791547f7740f
Isuru Fernando <isuruf@gmail.com>
2022-06-09 11:56:54 -0500

    Test that extending setuptools' build sub_commands does not extend distutils

--- setuptools/tests/test_build.py
+++ setuptools/tests/test_build.py
@@ -1,5 +1,6 @@
 from setuptools.dist import Distribution
 from setuptools.command.build import build
+from distutils.command.build import build as distutils_build
 
 
 def test_distribution_gives_setuptools_build_obj(tmpdir_cwd):
@@ -13,4 +14,11 @@ def test_distribution_gives_setuptools_build_obj(tmpdir_cwd):
         packages=[''],
         package_data={'': ['path/*']},
     ))
-    assert isinstance(dist.get_command_obj("build"), build)
+
+    build_obj = dist.get_command_obj("build")
+    assert isinstance(build_obj, build)
+
+    build_obj.sub_commands.append(("custom_build_subcommand", None))
+
+    distutils_subcommands = [cmd[0] for cmd in distutils_build.sub_commands]
+    assert "custom_build_subcommand" not in distutils_subcommands
