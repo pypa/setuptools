13108ed5d027e3519f28eacbe76ec18432f310dc
Clément Robert <cr52@protonmail.com>
2022-07-19 17:19:56 +0200

    Apply suggestions from code review
    Co-authored-by: Anderson Bravalheri <andersonbravalheri+github@gmail.com>

--- setuptools/ccompiler.py
+++ setuptools/ccompiler.py
@@ -1,6 +1,18 @@
 # Expose a subset of distutils as public API
 # to help with migration to Python 3.12
 
-from ._distutils.ccompiler import CCompiler
-from ._distutils.sysconfig import customize_compiler
-from ._distutils.ccompiler import new_compiler
+from distutils.ccompiler import CCompiler
+from distutils.ccompiler import new_compiler
+
+
+def customize_compiler(compiler: CCompiler):
+    """Do any platform-specific customization of a CCompiler instance.
+
+    Mainly needed on Unix, so we can plug in the information that
+    varies across Unices and is stored in Python's Makefile.
+    """
+    # Importing `distutils.sysconfig` directly may change the global state.
+    # We adopt a lazy approach instead.
+    from distutils.sysconfig import customize_compiler as _customize
+
+    return _customize(compiler)
