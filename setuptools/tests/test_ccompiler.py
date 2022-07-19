d0c49cfd6e38721e5a64533ed7f55645b4271d0b
Clément Robert <cr52@protonmail.com>
2022-07-19 17:20:12 +0200

    Apply suggestions from code review
    Co-authored-by: Anderson Bravalheri <andersonbravalheri+github@gmail.com>

--- setuptools/tests/test_ccompiler.py
+++ setuptools/tests/test_ccompiler.py
@@ -2,7 +2,23 @@ from setuptools.ccompiler import CCompiler
 from setuptools.ccompiler import customize_compiler
 from setuptools.ccompiler import new_compiler
 
-def test_ccompiler_namespace():
+import pytest
+
+
+def test_ccompiler_namespace(_avoid_permanent_changes_in_sysconfig):
     ccompiler = new_compiler()
     customize_compiler(ccompiler)
     assert hasattr(ccompiler, "compile")
+
+
+@pytest.fixture
+def _avoid_permanent_changes_in_sysconfig(monkeypatch):
+    import importlib
+    import sys
+
+    # Avoid caching `distutils.sysconfig` and force it to be re-imported later.
+    # This should "cancel out" any permanent changes that comes as a side-effect of
+    # import thing `distutils.sysconfig`.
+    monkeypatch.setattr(sys, "modules", sys.modules.copy())
+    yield
+    importlib.invalidate_caches()
