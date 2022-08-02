2b7c44fd88d5bc04bd4c68232231eeb190e3e2e3
Anderson Bravalheri <andersonbravalheri@gmail.com>
2022-08-02 12:47:12 +0100

    Add prepare_metadata_for_build_editable to build_meta.__all__

--- setuptools/build_meta.py
+++ setuptools/build_meta.py
@@ -52,6 +52,7 @@ __all__ = ['get_requires_for_build_sdist',
            'build_wheel',
            'build_sdist',
            'get_requires_for_build_editable',
+           'prepare_metadata_for_build_editable',
            'build_editable',
            '__legacy__',
            'SetupRequirementsError']
