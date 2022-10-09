a2d5c43368573f0fe0a620c87ef4dd9391d40eda
Jason R. Coombs <jaraco@jaraco.com>
2022-10-08 20:57:44 -0400

    Restore protection of distutils.log patching.

--- setuptools/logging.py
+++ setuptools/logging.py
@@ -24,6 +24,11 @@ def configure():
         format="{message}", style='{', handlers=handlers, level=logging.DEBUG)
     if hasattr(distutils.log, 'Log'):
         monkey.patch_func(set_threshold, distutils.log, 'set_threshold')
+        # For some reason `distutils.log` module is getting cached in `distutils.dist`
+        # and then loaded again when patched,
+        # implying: id(distutils.log) != id(distutils.dist.log).
+        # Make sure the same module object is used everywhere:
+        distutils.dist.log = distutils.log
 
 
 def set_threshold(level):
