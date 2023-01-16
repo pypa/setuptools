3c177109e36ec3e14db61d6164dac18e1b0490b1
Yevhenii Sevostianov <eugene.sevostianov@snapchat.com>
2023-01-16 13:49:42 -0800

    nits

--- tools/build_launchers.py
+++ tools/build_launchers.py
@@ -8,6 +8,27 @@ BUILD_PLATFORMS = ["Win32", "x64", "arm64"]
 REPO_ROOT = Path(__file__).parent.parent.resolve()
 LAUNCHER_CMAKE_PROJECT = REPO_ROOT / "launcher"
 MSBUILD_OUT_DIR = REPO_ROOT / "setuptools"
+"""
+Might be modified to visual studio that currently installed on the machine.
+Not tested with the older visual studios less then 16 version.
+Generators
+* Visual Studio 17 2022        = Generates Visual Studio 2022 project files.
+                                 Use -A option to specify architecture.
+  Visual Studio 16 2019        = Generates Visual Studio 2019 project files.
+                                 Use -A option to specify architecture.
+  Visual Studio 15 2017 [arch] = Generates Visual Studio 2017 project files.
+                                 Optional [arch] can be "Win64" or "ARM".
+  Visual Studio 14 2015 [arch] = Generates Visual Studio 2015 project files.
+                                 Optional [arch] can be "Win64" or "ARM".
+  Visual Studio 12 2013 [arch] = Generates Visual Studio 2013 project files.
+                                 Optional [arch] can be "Win64" or "ARM".
+  Visual Studio 11 2012 [arch] = Deprecated.  Generates Visual Studio 2012
+                                 project files.  Optional [arch] can be
+                                 "Win64" or "ARM".
+  Visual Studio 9 2008 [arch]  = Generates Visual Studio 2008 project files.
+                                 Optional [arch] can be "Win64" or "IA64".
+"""
+VISUAL_STUDIO_VERSION = "Visual Studio 17 2022"
 
 
 def get_executable_name(name, platform: str):
@@ -17,7 +38,7 @@ def get_executable_name(name, platform: str):
 
 
 def generate_cmake_project(build_arena, cmake_project_path, platform):
-    subprocess.check_call(f'cmake -G "Visual Studio 17 2022" -A "{platform}"'
+    subprocess.check_call(f'cmake -G "{VISUAL_STUDIO_VERSION}" -A "{platform}"'
                           f' {cmake_project_path}', shell=True, cwd=build_arena)
 
 
