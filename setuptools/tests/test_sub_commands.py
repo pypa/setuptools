aa49f2188179436ed605805de05ad9b526c584e9
Anderson Bravalheri <andersonbravalheri@gmail.com>
2023-02-27 16:15:49 +0000

    Update sub-commands test to capture new expectations

--- setuptools/tests/test_sub_commands.py
+++ setuptools/tests/test_sub_commands.py
@@ -25,36 +25,40 @@ class Cmd(Command):
         pass
 
 
-class Step0(Cmd):
-    before = "build_py"
+class StepPrepend(Cmd):
+    @staticmethod
+    def insert_build_step(steps, entry):
+        steps.insert(0, entry)
 
     def run(self):
-        assert not Path(self.distribution.src_root, "step1.run").exists()
-        Path(self.distribution.src_root, "step0.run").touch()
+        assert not Path(self.distribution.src_root, "step-append.run").exists()
+        Path(self.distribution.src_root, "step-prepend.run").touch()
 
 
-class Step1(Cmd):
-    after = "step0"
+class StepSuperseded(Cmd):
+    def run(self):
+        Path(self.distribution.src_root, "step-superseded.run").touch()
+        raise AssertionError("Should not have been called")
+
+
+class StepAppend(Cmd):
+    priority = 100
+    # Append by default, no need for insert_build_step
 
     def run(self):
-        assert Path(self.distribution.src_root, "step0.run").exists()
-        Path(self.distribution.src_root, "step1.run").touch()
-
-
-class Step2(Cmd):
-    # no after or before => last sub command
-
-    def run(self):
-        assert Path(self.distribution.src_root, "step0.run").exists()
-        assert Path(self.distribution.src_root, "step1.run").exists()
-        Path(self.distribution.src_root, "step2.run").touch()
+        assert Path(self.distribution.src_root, "step-prepend.run").exists()
+        Path(self.distribution.src_root, "step-append.run").touch()
 
 
 def test_custom_build_sub_commands(monkeypatch, tmp_path):
     with replace_entry_points(
         monkeypatch,
-        "setuptools.sub_commands.build",
-        {"step0": Step0, "step1": Step1, "step2": Step2}
+        "setuptools.build_steps",
+        [
+            ("step_append", StepAppend),
+            ("step_append", StepSuperseded),
+            ("step_prepend", StepPrepend),
+        ]
     ):
         attrs = {**BASE_DIST_EXAMPLE, "src_root": str(tmp_path)}
         dist = Distribution(attrs)
@@ -62,14 +66,12 @@ def test_custom_build_sub_commands(monkeypatch, tmp_path):
         sub_cmds = tuple(build.get_sub_commands())
 
         # Make sure ``after`` and ``before`` result in correct order
-        assert sub_cmds[0] == "step0"
-        assert sub_cmds[1] == "step1"
-        assert sub_cmds[-1] == "step2"
+        assert sub_cmds[0] == "step_prepend"
+        assert sub_cmds[-1] == "step_append"
 
         # Make sure sub command classes are registered
-        assert dist.get_command_class("step0") == Step0
-        assert dist.get_command_class("step1") == Step1
-        assert dist.get_command_class("step2") == Step2
+        assert dist.get_command_class("step_prepend") == StepPrepend
+        assert dist.get_command_class("step_append") == StepAppend
 
         # Make sure commands run correctly
         here = os.getcwd()
@@ -79,16 +81,16 @@ def test_custom_build_sub_commands(monkeypatch, tmp_path):
         finally:
             os.chdir(here)
 
-        assert (tmp_path / "step0.run").exists()
-        assert (tmp_path / "step1.run").exists()
-        assert (tmp_path / "step2.run").exists()
+        assert (tmp_path / "step-prepend.run").exists()
+        assert (tmp_path / "step-append.run").exists()
+        assert not (tmp_path / "step-superseded.run").exists()
 
 
 @contextmanager
 def replace_entry_points(monkeypatch, replaced_group, values):
     """Replace an specific entry-point group in importlib.metadata"""
     eps = []
-    for name, cls in values.items():
+    for name, cls in values:
         ep = Mock()
         ep.name = name
         ep.load = Mock(return_value=cls)
