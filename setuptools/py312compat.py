870268f0c57d483c0066fff661db01c33f6c3614
Anderson Bravalheri <andersonbravalheri@gmail.com>
2023-04-20 09:53:24 +0100

    Fix flake8 problem

--- setuptools/py312compat.py
+++ setuptools/py312compat.py
@@ -1,6 +1,7 @@
 import sys
 import shutil
 
+
 def shutil_rmtree(path, ignore_errors=False, onexc=None):
     if sys.version_info >= (3, 12):
         return shutil.rmtree(path, ignore_errors, onexc=onexc)
