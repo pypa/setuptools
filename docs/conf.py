c29955f9be8e44b2ea5fea12f86b7bd46a0b3958
Jason R. Coombs <jaraco@jaraco.com>
2023-07-04 11:53:15 -0400

    Collapse skeleton history. Workaround for jaraco/skeleton#87.

---
+++ docs/conf.py
@@ -0,0 +1,42 @@
+extensions = [
+    'sphinx.ext.autodoc',
+    'jaraco.packaging.sphinx',
+]
+
+master_doc = "index"
+html_theme = "furo"
+
+# Link dates and other references in the changelog
+extensions += ['rst.linker']
+link_files = {
+    '../NEWS.rst': dict(
+        using=dict(GH='https://github.com'),
+        replace=[
+            dict(
+                pattern=r'(Issue #|\B#)(?P<issue>\d+)',
+                url='{package_url}/issues/{issue}',
+            ),
+            dict(
+                pattern=r'(?m:^((?P<scm_version>v?\d+(\.\d+){1,2}))\n[-=]+\n)',
+                with_scm='{text}\n{rev[timestamp]:%d %b %Y}\n',
+            ),
+            dict(
+                pattern=r'PEP[- ](?P<pep_number>\d+)',
+                url='https://peps.python.org/pep-{pep_number:0>4}/',
+            ),
+        ],
+    )
+}
+
+# Be strict about any broken references
+nitpicky = True
+
+# Include Python intersphinx mapping to prevent failures
+# jaraco/skeleton#51
+extensions += ['sphinx.ext.intersphinx']
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+}
+
+# Preserve authored syntax for defaults
+autodoc_preserve_defaults = True
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+Welcome to |project| documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 1
+
+   history
+
+
+.. automodule:: PROJECT
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
