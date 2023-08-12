32a776ebabd9ca416886562e35c27015c0a2fd74
ruro <ruro.ruro@ya.ru>
2023-08-12 17:57:42 +0300

    reimplement _get_internal_depends according to the new logic

--- setuptools/command/build_ext.py
+++ setuptools/command/build_ext.py
@@ -11,7 +11,6 @@ from distutils.ccompiler import new_compiler
 from distutils.sysconfig import customize_compiler, get_config_var
 from distutils import log
 
-from setuptools import _path
 from setuptools.errors import BaseError
 from setuptools.extension import Extension, Library
 
@@ -270,11 +269,37 @@ class build_ext(_build_ext):
         """Yield ``ext.depends`` that are contained by the project directory"""
         project_root = Path(self.distribution.src_root or os.curdir).resolve()
         depends = (dep for ext in self.extensions for dep in ext.depends)
+
+        def skip(orig_path: str, reason: str) -> None:
+            log.info(
+                "dependency %s won't be automatically "
+                "included in the manifest: the path %s",
+                orig_path,
+                reason,
+            )
+
         for dep in depends:
+            path = Path(dep)
+
+            if path.is_absolute():
+                skip(dep, "must be relative")
+                continue
+
+            if ".." in path.parts:
+                skip(dep, "can't have `..` segments")
+                continue
+
             try:
-                yield _path.besteffort_internal_path(project_root, dep)
-            except ValueError:
-                log.warn(f"ignoring {dep} for distribution: outside of project dir")
+                resolved = (project_root / path).resolve(strict=True)
+            except OSError:
+                skip(dep, "doesn't exist")
+                continue
+
+            if not resolved.is_relative_to(project_root):
+                skip(dep, "must be inside the project root")
+                continue
+
+            yield path.as_posix()
 
     def get_outputs(self) -> List[str]:
         if self.inplace:
