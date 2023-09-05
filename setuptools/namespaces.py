94aee535999facca9a594d48e311e4e70265c6cf
Anderson Bravalheri <andersonbravalheri@gmail.com>
2023-09-05 10:33:18 +0100

    Avoid repeating namespace names in '*-nspkg.pth' file

--- setuptools/namespaces.py
+++ setuptools/namespaces.py
@@ -75,7 +75,7 @@ class Installer:
     def _get_all_ns_packages(self):
         """Return sorted list of all package namespaces"""
         pkgs = self.distribution.namespace_packages or []
-        return sorted(flatten(map(self._pkg_names, pkgs)))
+        return sorted(set(flatten(map(self._pkg_names, pkgs))))
 
     @staticmethod
     def _pkg_names(pkg):
