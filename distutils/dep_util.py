c4e27db944fc8ef08b215e593bbd328ce17bfff5
Jason R. Coombs <jaraco@jaraco.com>
2023-11-04 12:35:52 -0400

    "Refactor to newer_group to utilize higher level constructs ("any"), re-use _newer logic, and avoid complexity in branching."

--- distutils/dep_util.py
+++ distutils/dep_util.py
@@ -6,6 +6,16 @@ import stat
 from .errors import DistutilsFileError
 
 
+def _newer(source, target):
+    if not os.path.exists(target):
+        return True
+
+    mtime1 = os.stat(source)[stat.ST_MTIME]
+    mtime2 = os.stat(target)[stat.ST_MTIME]
+
+    return mtime1 > mtime2
+
+
 def newer(source, target):
     """
     Is source modified more recently than target.
@@ -18,13 +28,7 @@ def newer(source, target):
     if not os.path.exists(source):
         raise DistutilsFileError("file '%s' does not exist" % os.path.abspath(source))
 
-    if not os.path.exists(target):
-        return True
-
-    mtime1 = os.stat(source)[stat.ST_MTIME]
-    mtime2 = os.stat(target)[stat.ST_MTIME]
-
-    return mtime1 > mtime2
+    return _newer(source, target)
 
 
 def newer_pairwise(sources, targets):
@@ -66,26 +70,12 @@ def newer_group(sources, target, missing='error'):
       commands that wouldn't work because inputs are missing, but
       that doesn't matter because dry-run won't run the commands.
     """
-    # If the target doesn't even exist, then it's definitely out-of-date.
-    if not os.path.exists(target):
-        return True
 
-    # If *any* source file
-    # is more recent than 'target', then 'target' is out-of-date and
-    # we can immediately return True. If the loop completes, then
-    # 'target' is up-to-date.
-    target_mtime = os.stat(target)[stat.ST_MTIME]
-    for source in sources:
-        if not os.path.exists(source):
-            if missing == 'error':  # blow up when we stat() the file
-                pass
-            elif missing == 'ignore':  # missing source dropped from
-                continue  # target's dependency list
-            elif missing == 'newer':  # missing source means target is
-                return True  # out-of-date
+    def missing_as_newer(source):
+        return missing == 'newer' and not os.path.exists(source)
 
-        source_mtime = os.stat(source)[stat.ST_MTIME]
-        if source_mtime > target_mtime:
-            return True
-    else:
-        return False
+    ignored = os.path.exists if missing == 'ignore' else None
+    return any(
+        missing_as_newer(source) or _newer(source, target)
+        for source in filter(ignored, sources)
+    )
