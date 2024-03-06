d1019e1affec66ac105d7cecc14a4795e3365377
Anderson Bravalheri <andersonbravalheri@gmail.com>
2024-03-06 18:33:22 +0000

    Refactor test_pyproject.toml to use more jaraco.path.build

--- setuptools/tests/config/test_pyprojecttoml.py
+++ setuptools/tests/config/test_pyprojecttoml.py
@@ -357,10 +357,11 @@ def test_include_package_data_in_setuppy(tmp_path):
 
     See https://github.com/pypa/setuptools/issues/3197#issuecomment-1079023889
     """
-    pyproject = tmp_path / "pyproject.toml"
-    pyproject.write_text("[project]\nname = 'myproj'\nversion='42'\n")
-    setuppy = tmp_path / "setup.py"
-    setuppy.write_text("__import__('setuptools').setup(include_package_data=False)")
+    files = {
+        "pyproject.toml": "[project]\nname = 'myproj'\nversion='42'\n",
+        "setup.py": "__import__('setuptools').setup(include_package_data=False)",
+    }
+    path_build(files, prefix=tmp_path)
 
     with Path(tmp_path):
         dist = distutils.core.run_setup("setup.py", {}, stop_after="config")
