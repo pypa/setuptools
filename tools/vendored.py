fd5f55ea008b32d427d7059799302d65fd0cd0cd
Jason R. Coombs <jaraco@jaraco.com>
2024-04-12 12:35:18 -0400

    Refresh unpinned vendored dependencies.
    Closes #4253

--- tools/vendored.py
+++ tools/vendored.py
@@ -42,7 +42,12 @@ def rewrite_jaraco_text(pkg_files, new_root):
         file.write_text(text)
 
 
-def rewrite_jaraco(pkg_files, new_root):
+def repair_jaraco_namespace(pkg_files):
+    # required for zip-packaged setuptools #3084
+    pkg_files.joinpath('__init__.py').write_text('')
+
+
+def rewrite_jaraco_functools(pkg_files, new_root):
     """
     Rewrite imports in jaraco.functools to redirect to vendored copies.
     """
@@ -50,8 +55,6 @@ def rewrite_jaraco(pkg_files, new_root):
         text = file.read_text()
         text = re.sub(r' (more_itertools)', rf' {new_root}.\1', text)
         file.write_text(text)
-    # required for zip-packaged setuptools #3084
-    pkg_files.joinpath('__init__.py').write_text('')
 
 
 def rewrite_importlib_resources(pkg_files, new_root):
@@ -129,8 +132,9 @@ def update_pkg_resources():
     vendor = Path('pkg_resources/_vendor')
     install(vendor)
     rewrite_packaging(vendor / 'packaging', 'pkg_resources.extern')
+    repair_jaraco_namespace(vendor / 'jaraco')
     rewrite_jaraco_text(vendor / 'jaraco/text', 'pkg_resources.extern')
-    rewrite_jaraco(vendor / 'jaraco', 'pkg_resources.extern')
+    rewrite_jaraco_functools(vendor / 'jaraco/functools', 'pkg_resources.extern')
     rewrite_importlib_resources(vendor / 'importlib_resources', 'pkg_resources.extern')
     rewrite_more_itertools(vendor / "more_itertools")
     rewrite_platformdirs(vendor / "platformdirs")
@@ -140,8 +144,9 @@ def update_setuptools():
     vendor = Path('setuptools/_vendor')
     install(vendor)
     rewrite_packaging(vendor / 'packaging', 'setuptools.extern')
+    repair_jaraco_namespace(vendor / 'jaraco')
     rewrite_jaraco_text(vendor / 'jaraco/text', 'setuptools.extern')
-    rewrite_jaraco(vendor / 'jaraco', 'setuptools.extern')
+    rewrite_jaraco_functools(vendor / 'jaraco/functools', 'setuptools.extern')
     rewrite_importlib_resources(vendor / 'importlib_resources', 'setuptools.extern')
     rewrite_importlib_metadata(vendor / 'importlib_metadata', 'setuptools.extern')
     rewrite_more_itertools(vendor / "more_itertools")
