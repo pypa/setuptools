e74608b03526415568e4ba035166b66167a90df3
Jason R. Coombs <jaraco@jaraco.com>
2024-04-13 16:40:21 -0400

    Extend the retention of the compatibility.

--- distutils/compat/__init__.py
+++ distutils/compat/__init__.py
@@ -7,7 +7,7 @@ def consolidate_linker_args(args: list[str]) -> str:
     """
     Ensure the return value is a string for backward compatibility.
 
-    Retain until at least 2024-10-31.
+    Retain until at least 2024-04-31. See pypa/distutils#246
     """
 
     if not all(arg.startswith('-Wl,') for arg in args):
