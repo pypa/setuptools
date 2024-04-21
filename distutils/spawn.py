7babb5d5bfd477500f41ac5eecbee44591aed3e3
Jason R. Coombs <jaraco@jaraco.com>
2024-04-20 21:52:29 -0400

    Remove C901 exclusion; code is now compliant.

--- distutils/spawn.py
+++ distutils/spawn.py
@@ -15,7 +15,7 @@ from .debug import DEBUG
 from .errors import DistutilsExecError
 
 
-def spawn(cmd, search_path=1, verbose=0, dry_run=0, env=None):  # noqa: C901
+def spawn(cmd, search_path=1, verbose=0, dry_run=0, env=None):
     """Run another program, specified as a command list 'cmd', in a new process.
 
     'cmd' is just the argument list for the new process, ie.
