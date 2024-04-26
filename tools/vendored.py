836168ba18a743697cb9b9490afb3e87facde438
Anderson Bravalheri <andersonbravalheri@gmail.com>
2024-04-26 19:25:04 +0100

    Update vendoring mechanism

--- tools/vendored.py
+++ tools/vendored.py
@@ -7,83 +7,61 @@ from path import Path
 
 def remove_all(paths):
     for path in paths:
-        path.rmtree() if path.isdir() else path.remove()
+        path.rmtree() if path.is_dir() else path.remove()
 
 
 def update_vendored():
-    update_pkg_resources()
-    update_setuptools()
+    upgrade = "--upgrade" in sys.argv  # Very simple, no need for argparse yet.
+
+    update_target(
+        'setuptools',
+        upgrade=upgrade,
+        constraints=Path('pkg_resources/_vendor/vendored.txt'),  # ensure compatibility
+    )
+    update_target('pkg_resources', upgrade=upgrade)
 
 
-def rewrite_packaging(pkg_files, new_root):
+def repair_imports(vendor):
     """
-    Rewrite imports in packaging to redirect to vendored copies.
+    Rewrite imports to redirect to vendored copies.
+    This applies to all Python files inside the ``_vendor`` directory.
     """
-    for file in pkg_files.glob('*.py'):
-        text = file.text()
-        text = re.sub(r' (pyparsing)', rf' {new_root}.\1', text)
-        text = text.replace(
-            'from six.moves.urllib import parse',
-            'from urllib import parse',
-        )
-        file.write_text(text)
+    target = vendor.parent.name  # setuptools or pkg_resources
+    names = rf"(?P<name>{'|'.join(yield_top_level(target))})"
 
-
-def rewrite_jaraco_text(pkg_files, new_root):
-    """
-    Rewrite imports in jaraco.text to redirect to vendored copies.
-    """
-    for file in pkg_files.glob('*.py'):
+    for file in vendor.walkfiles(match="*.py"):
         text = file.read_text()
-        text = re.sub(r' (jaraco\.)', rf' {new_root}.\1', text)
-        text = re.sub(r' (importlib_resources)', rf' {new_root}.\1', text)
-        # suppress loading of lorem_ipsum; ref #3072
-        text = re.sub(r'^lorem_ipsum.*\n$', '', text, flags=re.M)
+        text = re.sub(  # import {name} => from setupools._vendor import {name}
+            rf"^(?P<indent>\s*)import\s+{names}(?P<remaining>.*)$",
+            rf'\g<indent>from {target}._vendor import \g<name>\g<remaining>',
+            # ^--- Assumes {name} is not nested.
+            #      Otherwise we need to pass `repl=<function>` to split the import
+            #      in multiple lines (one import for the parent and one for the child).
+            text,
+            flags=re.MULTILINE,
+        )
+        text = re.sub(  # from {name} => from setuptools._vendor.{name}
+            rf"^(?P<indent>\s*)from\s+{names}(?P<remaining>.* import .*)$",
+            rf'\g<indent>from {target}._vendor.\g<name>\g<remaining>',
+            text,
+            flags=re.MULTILINE,
+        )
         file.write_text(text)
 
 
 def repair_namespace(pkg_files):
     # required for zip-packaged setuptools #3084
-    pkg_files.joinpath('__init__.py').write_text('')
+    if pkg_files.is_dir():
+        pkg_files.joinpath('__init__.py').write_text('')
 
 
-def rewrite_jaraco_functools(pkg_files, new_root):
+def rewrite_jaraco_text(pkg_files):
     """
-    Rewrite imports in jaraco.functools to redirect to vendored copies.
+    Suppress loading of lorem_ipsum; ref #3072
     """
     for file in pkg_files.glob('*.py'):
         text = file.read_text()
-        text = re.sub(r' (more_itertools)', rf' {new_root}.\1', text)
-        file.write_text(text)
-
-
-def rewrite_jaraco_context(pkg_files, new_root):
-    """
-    Rewrite imports in jaraco.context to redirect to vendored copies.
-    """
-    for file in pkg_files.glob('context.py'):
-        text = file.read_text()
-        text = re.sub(r' (backports)', rf' {new_root}.\1', text)
-        file.write_text(text)
-
-
-def rewrite_importlib_resources(pkg_files, new_root):
-    """
-    Rewrite imports in importlib_resources to redirect to vendored copies.
-    """
-    for file in pkg_files.glob('*.py'):
-        text = file.read_text().replace('importlib_resources.abc', '.abc')
-        text = text.replace('zipp', '..zipp')
-        file.write_text(text)
-
-
-def rewrite_importlib_metadata(pkg_files, new_root):
-    """
-    Rewrite imports in importlib_metadata to redirect to vendored copies.
-    """
-    for file in pkg_files.glob('*.py'):
-        text = file.read_text().replace('typing_extensions', '..typing_extensions')
-        text = text.replace('import zipp', 'from .. import zipp')
+        text = re.sub(r'^lorem_ipsum.*\n$', '', text, flags=re.M)
         file.write_text(text)
 
 
@@ -92,6 +70,9 @@ def rewrite_more_itertools(pkg_files: Path):
     Defer import of concurrent.futures. Workaround for #3090.
     """
     more_file = pkg_files.joinpath('more.py')
+    if not more_file.exists():
+        return
+
     text = more_file.read_text()
     text = re.sub(r'^.*concurrent.futures.*?\n', '', text, flags=re.MULTILINE)
     text = re.sub(
@@ -102,35 +83,47 @@ def rewrite_more_itertools(pkg_files: Path):
     more_file.write_text(text)
 
 
-def rewrite_platformdirs(pkg_files: Path):
-    """
-    Replace some absolute imports with relative ones.
-    """
-    init = pkg_files.joinpath('__init__.py')
-    text = init.read_text()
-    text = text.replace('from platformdirs.', 'from .')
-    text = text.replace('from typing_extensions', 'from ..typing_extensions')
-    init.write_text(text)
-
-
 def clean(vendor):
     """
     Remove all files out of the vendor directory except the meta
     data (as pip uninstall doesn't support -t).
     """
-    ignored = ['vendored.txt', 'ruff.toml']
-    remove_all(path for path in vendor.glob('*') if path.basename() not in ignored)
+    ignored = ['vendored.in', 'vendored.txt', 'ruff.toml']
+    remove_all(path for path in vendor.glob('*') if path.name not in ignored)
 
 
-def install(vendor):
+def resolve(vendor, upgrade=False, constraints=None):
+    """Document why each indirect dependency is required."""
+    upgrade_args = ["--upgrade"] if upgrade else []
+    constraint_args = ["--constraint", str(constraints)] if constraints else []
+    args = [
+        sys.executable,
+        '-m',
+        'piptools',
+        'compile',
+        '--strip-extras',
+        *upgrade_args,
+        *constraint_args,
+        '-o',
+        str(vendor / 'vendored.txt'),
+        str(vendor / 'vendored.in'),
+    ]
+    subprocess.check_call(args)
+
+
+def install(vendor, constraints=None):
     clean(vendor)
+    constraint_args = ["--constraint", str(constraints)] if constraints else []
     install_args = [
         sys.executable,
         '-m',
         'pip',
         'install',
-        '-r',
+        *constraint_args,
+        '-c',
         str(vendor / 'vendored.txt'),
+        '-r',
+        str(vendor / 'vendored.in'),
         '-t',
         str(vendor),
     ]
@@ -138,46 +131,36 @@ def install(vendor):
     (vendor / '__init__.py').write_text('')
 
 
-def update_pkg_resources():
-    vendor = Path('pkg_resources/_vendor')
-    install(vendor)
-    rewrite_packaging(vendor / 'packaging', 'pkg_resources.extern')
+def update_target(target, upgrade=None, constraints=None):
+    vendor = Path(f'{target}/_vendor')
+    resolve(vendor, upgrade, constraints)
+    install(vendor, constraints)
+    repair_imports(vendor)  # Required for a vendoring workflow
+
+    # Patches related to issues
     repair_namespace(vendor / 'jaraco')
     repair_namespace(vendor / 'backports')
-    rewrite_jaraco_text(vendor / 'jaraco/text', 'pkg_resources.extern')
-    rewrite_jaraco_functools(vendor / 'jaraco/functools', 'pkg_resources.extern')
-    rewrite_jaraco_context(vendor / 'jaraco', 'pkg_resources.extern')
-    rewrite_importlib_resources(vendor / 'importlib_resources', 'pkg_resources.extern')
-    rewrite_more_itertools(vendor / "more_itertools")
-    rewrite_platformdirs(vendor / "platformdirs")
-
-
-def update_setuptools():
-    vendor = Path('setuptools/_vendor')
-    install(vendor)
-    rewrite_packaging(vendor / 'packaging', 'setuptools.extern')
-    repair_namespace(vendor / 'jaraco')
-    repair_namespace(vendor / 'backports')
-    rewrite_jaraco_text(vendor / 'jaraco/text', 'setuptools.extern')
-    rewrite_jaraco_functools(vendor / 'jaraco/functools', 'setuptools.extern')
-    rewrite_jaraco_context(vendor / 'jaraco', 'setuptools.extern')
-    rewrite_importlib_resources(vendor / 'importlib_resources', 'setuptools.extern')
-    rewrite_importlib_metadata(vendor / 'importlib_metadata', 'setuptools.extern')
+    rewrite_jaraco_text(vendor / 'jaraco/text')
     rewrite_more_itertools(vendor / "more_itertools")
 
 
-def yield_root_package(name):
-    """Useful when defining the MetaPathFinder
-    >>> examples = set(yield_root_package("setuptools")) & {"jaraco", "backports"}
+def yield_top_level(name):
+    """Iterate over all modules and (top level) packages vendored
+    >>> roots = set(yield_top_level("setuptools"))
+    >>> examples = roots & {"jaraco", "backports", "zipp"}
     >>> list(sorted(examples))
-    ['backports', 'jaraco']
+    ['backports', 'jaraco', 'zipp']
     """
-    vendored = Path(f"{name}/_vendor/vendored.txt")
-    yield from (
-        line.partition("=")[0].partition(".")[0].replace("-", "_")
-        for line in vendored.read_text(encoding="utf-8").splitlines()
-        if line and not line.startswith("#")
-    )
+    vendor = Path(f"{name}/_vendor")
+    ignore = {"__pycache__", "__init__.py", ".ruff_cache"}
+
+    for item in vendor.iterdir():
+        if item.name in ignore:
+            continue
+        if item.is_dir() and item.suffix != ".dist-info":
+            yield str(item.name)
+        if item.is_file() and item.suffix == ".py":
+            yield str(item.stem)
 
 
 __name__ == '__main__' and update_vendored()
