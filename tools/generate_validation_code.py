b1386b03b57ab0da05b8131f65856fd17cb9b805
Anderson Bravalheri <andersonbravalheri@gmail.com>
2024-05-17 14:13:16 +0100

    Fix typing errors

--- tools/generate_validation_code.py
+++ tools/generate_validation_code.py
@@ -4,9 +4,10 @@ import itertools
 import subprocess
 import sys
 from pathlib import Path
+from typing import Iterable
 
 
-def generate_pyproject_validation(dest: Path, schemas: Path):
+def generate_pyproject_validation(dest: Path, schemas: Iterable[Path]) -> bool:
     """
     Generates validation code for ``pyproject.toml`` based on JSON schemas and the
     ``validate-pyproject`` library.
@@ -25,10 +26,11 @@ def generate_pyproject_validation(dest: Path, schemas: Path):
     ]
     subprocess.check_call(cmd)
     print(f"Validation code generated at: {dest}")
+    return True
 
 
-def main():
-    generate_pyproject_validation(
+def main() -> bool:
+    return generate_pyproject_validation(
         Path("setuptools/config/_validate_pyproject"),
         schemas=Path("setuptools/config").glob("*.schema.json"),
     )
