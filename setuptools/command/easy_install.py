70b23105f911a3909dd5640f8de3ae5b07f6f86e
Dimitri Papadopoulos Orfanos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2024-06-29 08:09:10 +0200

    Update setuptools/command/easy_install.py
    Co-authored-by: Avasam <samuel.06@hotmail.com>

--- setuptools/command/easy_install.py
+++ setuptools/command/easy_install.py
@@ -1109,9 +1109,7 @@ class easy_install(Command):
             self.execute(
                 wheel.install_as_egg,
                 (destination,),
-                (
-                    f"Installing {os.path.basename(wheel_path)} to {os.path.dirname(destination)}"
-                ),
+                f"Installing {os.path.basename(wheel_path)} to {os.path.dirname(destination)}",
             )
         finally:
             update_dist_caches(destination, fix_zipimporter_caches=False)
