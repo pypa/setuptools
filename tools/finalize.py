2ad959d4fe0e45ec75456bc6eca2e22ce91a6c92
Anderson Bravalheri <andersonbravalheri@gmail.com>
2024-08-01 14:56:48 +0100

    Use finalize to save stable version

--- tools/finalize.py
+++ tools/finalize.py
@@ -1,9 +1,8 @@
 """
-Finalize the repo for a release. Invokes towncrier and bumpversion.
+Finalize the repo for a release. Invokes towncrier.
 """
 
-__requires__ = ['bump2version', 'towncrier', 'jaraco.develop>=7.21']
-
+__requires__ = ['towncrier', 'jaraco.develop>=7.23']
 
 import pathlib
 import re
@@ -21,9 +20,13 @@ bump_version_command = [
 
 
 def get_version():
-    cmd = bump_version_command + ['--dry-run', '--verbose']
-    out = subprocess.check_output(cmd, text=True, encoding='utf-8')
-    return re.search('^new_version=(.*)', out, re.MULTILINE).group(1)
+    return towncrier.semver(towncrier.get_version())
+
+
+def save_version(version):
+    pathlib.Path(".version").unlink()  # Remove development version
+    pathlib.Path(".stable").write_text(version, encoding="utf-8")
+    subprocess.check_output(['git', 'add', ".stable"])
 
 
 def update_changelog():
@@ -42,11 +45,6 @@ def _repair_changelog():
     subprocess.check_output(['git', 'add', changelog_fn])
 
 
-def bump_version():
-    cmd = bump_version_command + ['--allow-dirty']
-    subprocess.check_call(cmd)
-
-
 def ensure_config():
     """
     Double-check that Git has an e-mail configured.
@@ -55,8 +53,11 @@ def ensure_config():
 
 
 if __name__ == '__main__':
-    print("Cutting release at", get_version())
+    version = get_version()
+    print("Cutting release at", version)
     ensure_config()
+    save_version(version)
     towncrier.check_changes()
     update_changelog()
-    bump_version()
+    subprocess.check_call(['git', 'commit', '-a', '-m', f'Finalize #{version}'])
+    subprocess.check_call(['git', 'tag', '-a', '-m', '', version])
