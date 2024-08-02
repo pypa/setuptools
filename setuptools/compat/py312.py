f14509d2ef9998f860564c6a2b0eef7882777c7c
Anderson Bravalheri <andersonbravalheri+github@gmail.com>
2024-08-02 11:59:06 +0100

    Update setuptools/compat/py312.py
    Co-authored-by: Avasam <samuel.06@hotmail.com>

--- setuptools/compat/py312.py
+++ setuptools/compat/py312.py
@@ -1,6 +1,6 @@
 import sys
 
-if sys.version_info >= (3, 13):
+if sys.version_info >= (3, 12, 4):
     # Python 3.13 should support `.pth` files encoded in UTF-8
     # See discussion in https://github.com/python/cpython/issues/77102
     PTH_ENCODING = "utf-8"
