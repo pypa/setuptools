3961909a430485f92a70a6ba47eb76e43cf2a579
Anderson Bravalheri <andersonbravalheri@gmail.com>
2024-08-12 19:01:16 +0100

    Add todo annotation

--- setuptools/config/_apply_pyprojecttoml.py
+++ setuptools/config/_apply_pyprojecttoml.py
@@ -278,6 +278,7 @@ def _valid_command_options(cmdclass: Mapping = EMPTY) -> dict[str, set[str]]:
 def _load_ep(ep: metadata.EntryPoint) -> tuple[str, type] | None:
     if ep.value.startswith("wheel.bdist_wheel"):
         # Ignore deprecated entrypoint from wheel and avoid warning pypa/wheel#631
+        # TODO: remove check when `bdist_wheel` has been fully removed from pypa/wheel
         return None
 
     # Ignore all the errors
