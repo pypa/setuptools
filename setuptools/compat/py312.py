353d0aabca8f655ee369ba41d9072bc0e9292a37
Anderson Bravalheri <andersonbravalheri@gmail.com>
2024-08-12 17:26:21 +0100

    Fix type check

--- setuptools/compat/py312.py
+++ setuptools/compat/py312.py
@@ -1,9 +1,11 @@
+from __future__ import annotations
+
 import sys
 
 if sys.version_info >= (3, 12, 4):
     # Python 3.13 should support `.pth` files encoded in UTF-8
     # See discussion in https://github.com/python/cpython/issues/77102
-    PTH_ENCODING = "utf-8"
+    PTH_ENCODING: str | None = "utf-8"
 else:
     from .py39 import LOCALE_ENCODING
 
