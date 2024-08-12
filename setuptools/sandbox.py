7aa30d0d4da1db84a32d1d1e9afab49a20b1c717
Avasam <samuel.06@hotmail.com>
2024-08-12 11:38:03 -0400

    Remove sandbox Python 2 builtins.file support (#4554)

--- setuptools/sandbox.py
+++ setuptools/sandbox.py
@@ -22,10 +22,6 @@ if sys.platform.startswith('java'):
     import org.python.modules.posix.PosixModule as _os
 else:
     _os = sys.modules[os.name]
-try:
-    _file = file  # type: ignore[name-defined] # Check for global variable
-except NameError:
-    _file = None
 _open = open
 
 
@@ -285,15 +281,11 @@ class AbstractSandbox(ABC):
 
     def __enter__(self):
         self._copy(self)
-        if _file:
-            builtins.file = self._file
         builtins.open = self._open
         self._active = True
 
     def __exit__(self, exc_type, exc_value, traceback):
         self._active = False
-        if _file:
-            builtins.file = _file
         builtins.open = _open
         self._copy(_os)
 
@@ -326,8 +318,6 @@ class AbstractSandbox(ABC):
 
         return wrap
 
-    if _file:
-        _file = _mk_single_path_wrapper('file', _file)
     _open = _mk_single_path_wrapper('open', _open)
     for __name in [
         "stat",
@@ -443,13 +433,6 @@ class DirectorySandbox(AbstractSandbox):
         from setuptools.sandbox import SandboxViolation
 
         raise SandboxViolation(operation, args, kw)
-
-    if _file:
-
-        def _file(self, path, mode='r', *args, **kw):
-            if mode not in ('r', 'rt', 'rb', 'rU', 'U') and not self._ok(path):
-                self._violation("file", path, mode, *args, **kw)
-            return _file(path, mode, *args, **kw)
 
     def _open(self, path, mode='r', *args, **kw):
         if mode not in ('r', 'rt', 'rb', 'rU', 'U') and not self._ok(path):
