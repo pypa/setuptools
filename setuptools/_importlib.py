84764b4d0bd222fc9bd59e141ee2cdf6d9cbdf79
Anderson Bravalheri <andersonbravalheri@gmail.com>
2024-08-15 14:52:04 +0100

    Add exception for old versions of importlib_resources

--- setuptools/_importlib.py
+++ setuptools/_importlib.py
@@ -10,3 +10,10 @@ if sys.version_info < (3, 9):
     import importlib_resources as resources  # pragma: no cover
 else:
     import importlib.resources as resources  # noqa: F401
+
+
+if not hasattr(metadata, "EntryPoints"):  # pragma: no cover
+    raise ImportError(
+        "`importlib-metadata` version is incompatible with `setuptools`. "
+        "Please install importlib-metadata>=4.6"  # Equivalent to stdlib's in 3.10
+    )
