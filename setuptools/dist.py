49d7438a23b7ec7cfca20c4d0ae5083030f631bd
Anderson Bravalheri <andersonbravalheri@gmail.com>
2024-08-16 15:28:12 +0100

    Replace OrderedSet with dict
    Since Python 3.7, ``dict`` preserve insertion order.
    This means that ``dict`` is a capable implementation of an ordered set,
    despite the method names not lining up completely.

--- setuptools/dist.py
+++ setuptools/dist.py
@@ -11,7 +11,6 @@ from pathlib import Path
 from typing import TYPE_CHECKING, MutableMapping
 
 from more_itertools import partition, unique_everseen
-from ordered_set import OrderedSet
 from packaging.markers import InvalidMarker, Marker
 from packaging.specifiers import InvalidSpecifier, SpecifierSet
 from packaging.version import Version
@@ -251,7 +250,7 @@ class Distribution(_Distribution):
     _DISTUTILS_UNSUPPORTED_METADATA = {
         'long_description_content_type': lambda: None,
         'project_urls': dict,
-        'provides_extras': OrderedSet,
+        'provides_extras': dict,  # behaves like an ordered set
         'license_file': lambda: None,
         'license_files': lambda: None,
         'install_requires': list,
@@ -349,7 +348,7 @@ class Distribution(_Distribution):
                 # Setuptools allows a weird "<name>:<env markers> syntax for extras
                 extra = extra.split(':')[0]
                 if extra:
-                    self.metadata.provides_extras.add(extra)
+                    self.metadata.provides_extras.setdefault(extra)
 
     def _normalize_requires(self):
         """Make sure requirement-related attributes exist and are normalized"""
