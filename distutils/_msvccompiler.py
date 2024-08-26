a46c2401187c80b8647d3f3d049bed96c88b054d
Kagami Sascha Rosylight <saschanaz@outlook.com>
2024-08-26 18:57:54 +0200

    nit

--- distutils/_msvccompiler.py
+++ distutils/_msvccompiler.py
@@ -182,7 +182,7 @@ def _get_plat_to_vcvars():
     # A map keyed by get_platform() return values to values accepted by
     # 'vcvarsall.bat'.
     host_platform = get_host_platform()
-    if host_platform == get_platform() and get_host_platform() == "win-arm64":
+    if host_platform == get_platform() and host_platform == "win-arm64":
         # Use the native MSVC host if the host platform would need expensive
         # emulation for x86.
         return {
