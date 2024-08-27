2e2e918f7a6292bba2b4b0a40ba0d87c579dc334
Anderson Bravalheri <andersonbravalheri+github@gmail.com>
2024-08-27 11:11:54 +0100

    Remove unnecessary conditional on TYPE_CHECKING

--- setuptools/command/test.py
+++ setuptools/command/test.py
@@ -1,10 +1,5 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    pass
-
 from setuptools import Command
 from setuptools.warnings import SetuptoolsDeprecationWarning
 
