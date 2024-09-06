cdd4c28904fdc81f14941ac874127207a6966e04
Jason R. Coombs <jaraco@jaraco.com>
2024-09-06 13:24:14 -0400

    Direct tests to the new names

--- distutils/compilers/C/tests/test_msvc.py
+++ distutils/compilers/C/tests/test_msvc.py
@@ -1,19 +1,17 @@
-"""Tests for distutils._msvccompiler."""
-
 import os
 import sys
 import sysconfig
 import threading
 import unittest.mock as mock
-from distutils import _msvccompiler
-from distutils.compilers.C import msvc
 from distutils.errors import DistutilsPlatformError
 from distutils.tests import support
 from distutils.util import get_platform
 
 import pytest
 
-needs_winreg = pytest.mark.skipif('not hasattr(_msvccompiler, "winreg")')
+from .. import msvc
+
+needs_winreg = pytest.mark.skipif('not hasattr(msvc, "winreg")')
 
 
 class Testmsvccompiler(support.TempdirManager):
@@ -47,12 +45,12 @@ class Testmsvccompiler(support.TempdirManager):
         """
         Ensure a specified target platform is passed to _get_vcvars_spec.
         """
-        compiler = _msvccompiler.MSVCCompiler()
+        compiler = msvc.Compiler()
 
         def _get_vcvars_spec(host_platform, platform):
             assert platform == expected
 
-        monkeypatch.setattr(_msvccompiler, '_get_vcvars_spec', _get_vcvars_spec)
+        monkeypatch.setattr(msvc, '_get_vcvars_spec', _get_vcvars_spec)
         compiler.initialize(plat_name)
 
     @needs_winreg
@@ -64,7 +62,7 @@ class Testmsvccompiler(support.TempdirManager):
         old_distutils_use_sdk = os.environ.pop('DISTUTILS_USE_SDK', None)
         os.environ[test_var] = test_value
         try:
-            env = _msvccompiler._get_vc_env('x86')
+            env = msvc._get_vc_env('x86')
             assert test_var.lower() in env
             assert test_value == env[test_var.lower()]
         finally:
@@ -77,7 +75,7 @@ class Testmsvccompiler(support.TempdirManager):
     def test_get_vc(self, ver):
         # This function cannot be mocked, so pass if VC is found
         # and skip otherwise.
-        lookup = getattr(_msvccompiler, f'_find_vc{ver}')
+        lookup = getattr(msvc, f'_find_vc{ver}')
         expected_version = {2015: 14, 2017: 15}[ver]
         version, path = lookup()
         if not version:
@@ -104,7 +102,7 @@ class TestSpawn:
         """
         Concurrent calls to spawn should have consistent results.
         """
-        compiler = _msvccompiler.MSVCCompiler()
+        compiler = msvc.Compiler()
         compiler._paths = "expected"
         inner_cmd = 'import os; assert os.environ["PATH"] == "expected"'
         command = [sys.executable, '-c', inner_cmd]
@@ -125,7 +123,7 @@ class TestSpawn:
         """
         from distutils import ccompiler
 
-        compiler = _msvccompiler.MSVCCompiler()
+        compiler = msvc.Compiler()
         compiler._paths = "expected"
 
         def CCompiler_spawn(self, cmd):
