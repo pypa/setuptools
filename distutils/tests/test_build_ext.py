a88eace7acc39b76aeb8d967777d285dbeb0341f
Avasam <samuel.06@hotmail.com>
2024-11-24 17:08:38 -0500

    UP031 manual fixes for Ruff 0.8.0

--- distutils/tests/test_build_ext.py
+++ distutils/tests/test_build_ext.py
@@ -522,14 +522,15 @@ class TestBuildExt(TempdirManager):
         # at least one value we test with will not exist yet.
         if target[:2] < (10, 10):
             # for 10.1 through 10.9.x -> "10n0"
-            target = '%02d%01d0' % target
+            tmpl = '{:02}{:01}0'
         else:
             # for 10.10 and beyond -> "10nn00"
             if len(target) >= 2:
-                target = '%02d%02d00' % target
+                tmpl = '{:02}{:02}00'
             else:
                 # 11 and later can have no minor version (11 instead of 11.0)
-                target = '%02d0000' % target
+                tmpl = '{:02}0000'
+        target = tmpl.format(*target)
         deptarget_ext = Extension(
             'deptarget',
             [self.tmp_path / 'deptargetmodule.c'],
