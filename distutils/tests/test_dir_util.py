468532edd3ce99dfdbdf88d9a0b70a2b50fccc04
Jason R. Coombs <jaraco@jaraco.com>
2024-12-26 12:10:10 -0500

    👹 Feed the hobgoblins (delint).

--- distutils/tests/test_dir_util.py
+++ distutils/tests/test_dir_util.py
@@ -106,8 +106,9 @@ class TestDirUtil(support.TempdirManager):
         """
         An exception in listdir should raise a DistutilsFileError
         """
-        with mock.patch("os.listdir", side_effect=OSError()), pytest.raises(
-            errors.DistutilsFileError
+        with (
+            mock.patch("os.listdir", side_effect=OSError()),
+            pytest.raises(errors.DistutilsFileError),
         ):
             src = self.tempdirs[-1]
             dir_util.copy_tree(src, None)
