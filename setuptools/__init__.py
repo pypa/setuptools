0ac9dba8f0d03a688db2c75af343620e2afdb23d
Jason R. Coombs <jaraco@jaraco.com>
2025-03-16 12:05:33 -0400

    Ignore pyright check for `dry_run = False`.

--- setuptools/__init__.py
+++ setuptools/__init__.py
@@ -167,7 +167,7 @@ class Command(_Command):
     command_consumes_arguments = False
     distribution: Distribution  # override distutils.dist.Distribution with setuptools.dist.Distribution
 
-    dry_run = False
+    dry_run = False  # pyright: ignore[reportAssignmentType] # (until #4689; see #4872)
     """
     For compatibility with vendored bdist_wheel.
     https://github.com/pypa/setuptools/pull/4872/files#r1986395142
