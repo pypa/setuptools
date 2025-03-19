205a3ecc960f709d0811aa08f097b9537d36c107
maniacdc <ManiacDC@users.noreply.github.com>
2025-03-19 19:54:41 -0400

    * fixed import order * added documentation to imports as to why they are there, and when they can be removed.

--- distutils/ccompiler.py
+++ distutils/ccompiler.py
@@ -1,25 +1,28 @@
 from .compilers.C import base
 from .compilers.C.base import (
+    # `_default_compilers` is needed by numpy.distutils, which is supported until
+    #  Python 3.11 is deprecated. This import & export can be removed when
+    #  Python 3.11 is no longer supported by distutils.
+    _default_compilers,
     compiler_class,
     gen_lib_options,
     gen_preprocess_options,
     get_default_compiler,
     new_compiler,
     show_compilers,
-    _default_compilers,
 )
 from .compilers.C.errors import CompileError, LinkError
 
 __all__ = [
     "CompileError",
     "LinkError",
+    "_default_compilers",
     "compiler_class",
     "gen_lib_options",
     "gen_preprocess_options",
     "get_default_compiler",
     "new_compiler",
     "show_compilers",
-    "_default_compilers",
 ]
 
 
