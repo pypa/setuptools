20d2fc64b9c2d58b053b488d6dea79e2ce748950
Jason R. Coombs <jaraco@jaraco.com>
2025-04-20 16:39:51 -0400

    Decoupled develop from easy_install.

--- setuptools/command/develop.py
+++ setuptools/command/develop.py
@@ -1,30 +1,14 @@
-from setuptools import namespaces
-from setuptools.command.easy_install import easy_install
+from setuptools import Command, namespaces
 
 
-class develop(namespaces.DevelopInstaller, easy_install):
+class develop(namespaces.DevelopInstaller, Command):
     """Set up package for development"""
-
-    description = "install package in 'development mode'"
-
-    user_options = easy_install.user_options + [
-        ("uninstall", "u", "Uninstall this source package"),
-        ("egg-path=", None, "Set the path to be used in the .egg-link file"),
-    ]
-
-    boolean_options = easy_install.boolean_options + ['uninstall']
-
-    command_consumes_arguments = False  # override base
 
     def run(self):
         raise NotImplementedError
 
     def initialize_options(self):
-        self.uninstall = None
-        self.egg_path = None
-        easy_install.initialize_options(self)
-        self.setup_path = None
-        self.always_copy_from = '.'  # always copy eggs installed in curdir
+        "stubbed"
 
     def finalize_options(self) -> None:
         raise NotImplementedError
