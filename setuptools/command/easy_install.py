30c00380093b1a7ff5693f98d06ab4fa4f8923cf
Jason R. Coombs <jaraco@jaraco.com>
2025-05-04 12:41:44 -0400

    Render the attributes dynamically.

--- setuptools/command/easy_install.py
+++ setuptools/command/easy_install.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import types
 
 from setuptools import Command
 
@@ -10,5 +11,14 @@ class easy_install(Command):
     """Stubbed command for temporary pbr compatibility."""
 
 
-ScriptWriter = _scripts.ScriptWriter
-sys_executable = os.environ.get("__PYVENV_LAUNCHER__", os.path.normpath(sys.executable))
+def __getattr__(name):
+    attr = getattr(
+        types.SimpleNamespace(
+            ScriptWriter=_scripts.ScriptWriter,
+            sys_executable=os.environ.get(
+                "__PYVENV_LAUNCHER__", os.path.normpath(sys.executable)
+            ),
+        ),
+        name,
+    )
+    return attr
