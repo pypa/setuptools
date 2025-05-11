1bc752c84bc7b489d311feca2e89b2a902b90b87
Jason R. Coombs <jaraco@jaraco.com>
2025-05-11 15:31:42 -0400

    In build_ext, remove reliance on pkg_resources.

--- setuptools/command/build_ext.py
+++ setuptools/command/build_ext.py
@@ -356,25 +356,25 @@ class build_ext(_build_ext):
                     textwrap.dedent(f"""
                     def __bootstrap__():
                        global __bootstrap__, __file__, __loader__
-                       import sys, os, pkg_resources, importlib.util
+                       import sys, os, importlib.resources as irs, importlib.util
                     #rtld   import dl
-                       __file__ = pkg_resources.resource_filename(__name__,
-                         {os.path.basename(ext._file_name)!r})
-                       del __bootstrap__
-                       if '__loader__' in globals():
-                           del __loader__
-                    #rtld   old_flags = sys.getdlopenflags()
-                       old_dir = os.getcwd()
-                       try:
-                         os.chdir(os.path.dirname(__file__))
-                    #rtld     sys.setdlopenflags(dl.RTLD_NOW)
-                         spec = importlib.util.spec_from_file_location(
-                                    __name__, __file__)
-                         mod = importlib.util.module_from_spec(spec)
-                         spec.loader.exec_module(mod)
-                       finally:
-                    #rtld     sys.setdlopenflags(old_flags)
-                         os.chdir(old_dir)
+                       with irs.files(__name__).joinpath(
+                         {os.path.basename(ext._file_name)!r}) as __file__:
+                          del __bootstrap__
+                          if '__loader__' in globals():
+                              del __loader__
+                    #rtld      old_flags = sys.getdlopenflags()
+                          old_dir = os.getcwd()
+                          try:
+                            os.chdir(os.path.dirname(__file__))
+                    #rtld        sys.setdlopenflags(dl.RTLD_NOW)
+                            spec = importlib.util.spec_from_file_location(
+                                       __name__, __file__)
+                            mod = importlib.util.module_from_spec(spec)
+                            spec.loader.exec_module(mod)
+                          finally:
+                    #rtld        sys.setdlopenflags(old_flags)
+                            os.chdir(old_dir)
                     __bootstrap__()
                     """)
                     .lstrip()
