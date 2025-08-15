e590e62f64f1803cbaa43e25a6b302035857a5cc
Jason R. Coombs <jaraco@jaraco.com>
2025-08-15 14:21:42 -0700

    Restore test for pbr. Pin to newer version.

--- setuptools/tests/fixtures.py
+++ setuptools/tests/fixtures.py
@@ -384,7 +384,13 @@ def pbr_package(tmp_path, monkeypatch, venv):
         },
         "other": {"test.txt": "Another file in here."},
     }
-    venv.run(["python", "-m", "pip", "install", "pbr"])
+    venv.run([
+        "python",
+        "-m",
+        "pip",
+        "install",
+        "pbr@git+https://opendev.org/openstack/pbr@e7f140f9de722db8c7e788974f7feef9609bcaee",
+    ])
     prefix = tmp_path / 'mypkg'
     prefix.mkdir()
     jaraco.path.build(files, prefix=prefix)
