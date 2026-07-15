e773760e12e8036a3ab36e638f75d2b4b544f150
itscloud0 <cloud0beats@gmail.com>
2026-07-15 20:09:49 +0300

    Fix build_py warning test filters

--- setuptools/tests/test_build_py.py
+++ setuptools/tests/test_build_py.py
@@ -287,15 +287,14 @@ def test_importable_data_subpackage_warns_and_includes_files(tmpdir_cwd):
     build_py = dist.get_command_obj("build_py")
     msg = r"Package 'mypkg\.plugins' is absent from the `packages` configuration\."
     build_py.finalize_options()
-    with warnings.catch_warnings():
-        if os.getenv("SETUPTOOLS_USE_DISTUTILS") == "stdlib":
+    with pytest.warns(SetuptoolsDeprecationWarning, match=msg):
+        with warnings.catch_warnings():
             warnings.filterwarnings(
                 "ignore",
                 "'encoding' argument not specified",
                 module="distutils.text_file",
                 # This warning is already fixed in pypa/distutils but not in stdlib
             )
-        with pytest.warns(SetuptoolsDeprecationWarning, match=msg):
             build_py.run()
 
     build_dir = Path(dist.get_command_obj("build_py").build_lib)
